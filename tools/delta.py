2abc807d07b8c304407974865192d8ddc54f9f14
Stefan Westerfeld <stefan@space.twc.de>
2010-03-29 16:39:12 +0200

    Added a python script which computes the difference between two samples.

---
+++ tools/delta.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+
+def read_samples (filename):
+  samples = []
+  f = open (filename)
+  for line in f.readlines():
+    if line[0] != "#":      # skip comments
+      samples += [ float (line) ]
+  return samples
+
+a_samples = read_samples (sys.argv[1])
+b_samples = read_samples (sys.argv[2])
+
+for x in range (0, max (len (a_samples), len (b_samples))):
+  diff = 0
+  if x < len (a_samples):
+    diff += a_samples[x]
+  if x < len (b_samples):
+    diff -= b_samples[x]
+  print diff
