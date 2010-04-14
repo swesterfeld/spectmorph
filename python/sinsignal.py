5b7ed8171e45d065d0935610bd35285e89193153
Stefan Westerfeld <stefan@space.twc.de>
2010-04-14 14:00:11 +0200

    Added python based test signal generator.

---
+++ python/sinsignal.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+import math
+from math import sin
+
+partials = []
+rate = int (sys.argv[1])
+i = 2
+while i < len (sys.argv):
+  freq = float (sys.argv[i])
+  amp = float (sys.argv[i+1])
+  partials += [ (freq, amp) ]
+  i += 2
+
+phases = [ 0 ] * len (partials)
+for t in range (int (rate * 0.1)):                          # generate a 0.1 second test signal
+  value = 0
+  for p in range (len (partials)):
+    value += sin (phases[p]) * partials[p][1]
+    phases[p] += partials[p][0] * 2 * math.pi / rate
+  print value
