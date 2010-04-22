4c380daba90c10dbf6cc070a08491f9434a268a2
Stefan Westerfeld <stefan@space.twc.de>
2010-04-22 12:07:32 +0100

    Check for automake, autoconf, libtool and pkg-config

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
+
+if automake --version >/dev/null 2>&1; then
+  :
+else
+  echo "You need to have automake installed to build this package"
+  DIE=1
+fi
+
+if autoconf --version >/dev/null 2>&1; then
+  :
+else
+  echo "You need to have autoconf installed to build this package"
+  DIE=1
+fi
+
+if libtoolize --version >/dev/null 2>&1; then
+  :
+else
+  echo "You need to have libtool installed to build this package"
+  DIE=1
+fi
+
+# bail out as scheduled
+test "0$DIE" -gt 0 && exit 1
+
 autoreconf -i && ./configure "$@"
