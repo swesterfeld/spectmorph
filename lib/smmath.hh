feebad870804321238dd039a80cf6c49bd751eae
Stefan Westerfeld <stefan@space.twc.de>
2010-05-12 12:40:22 +0200

    Added SSE optimized version of sincos() vector function.

--- lib/smmath.hh
+++ lib/smmath.hh
@@ -20,6 +20,9 @@
 
 #include <math.h>
 #include <glib.h>
+#ifdef __SSE__
+#include <xmmintrin.h>
+#endif
 
 namespace SpectMorph
 {
@@ -42,7 +45,7 @@ VectorSinParams
 
 
 template<class Iterator>
-void
+inline void
 fast_vector_sin_add (const VectorSinParams& params, Iterator begin, Iterator end)
 {
   g_return_if_fail (params.mix_freq > 0 && params.freq > 0 && params.phase > -99 && params.mag > 0);
@@ -83,7 +86,7 @@ fast_vector_sin_add (const VectorSinParams& params, Iterator begin, Iterator end
 }
 
 template<class Iterator>
-void
+inline void
 fast_vector_sincos_add (const VectorSinParams& params, Iterator sin_begin, Iterator sin_end, Iterator cos_begin)
 {
   g_return_if_fail (params.mix_freq > 0 && params.freq > 0 && params.phase > -99 && params.mag > 0);
@@ -122,6 +125,99 @@ fast_vector_sincos_add (const VectorSinParams& params, Iterator sin_begin, Itera
           state_im = im;
         }
     }
+}
+
+/* see: http://ds9a.nl/gcc-simd/ */
+union F4Vector
+{
+  float f[4];
+#ifdef __SSE__
+  __m128 v;   // vector of four single floats
+#endif
+};
+
+inline void
+float_fast_vector_sincos_add (const VectorSinParams& params, float *sin_begin, float *sin_end, float *cos_begin)
+{
+#ifdef __SSE__
+  g_return_if_fail (params.mix_freq > 0 && params.freq > 0 && params.phase > -99 && params.mag > 0);
+
+  const int TABLE_SIZE = 32;
+
+  const double phase_inc = params.freq / params.mix_freq * 2 * M_PI;
+  const double inc_re16 = cos (phase_inc * TABLE_SIZE * 4);
+  const double inc_im16 = sin (phase_inc * TABLE_SIZE * 4);
+  int n = 0;
+
+  double state_re;
+  double state_im;
+
+  sincos (params.phase, &state_im, &state_re);
+  state_re *= params.mag;
+  state_im *= params.mag;
+
+
+  F4Vector incf_re[TABLE_SIZE] = {0, };
+  F4Vector incf_im[TABLE_SIZE] = {0, };
+
+  // compute tables using FPU
+  VectorSinParams table_params = params;
+  table_params.phase = 0;
+  table_params.mag = 1;
+  fast_vector_sincos_add (table_params, incf_im[0].f, incf_im[0].f + (TABLE_SIZE * 4), incf_re[0].f);
+
+  // inner loop using SSE instructions
+  int todo = sin_end - sin_begin;
+  while (todo >= 4 * TABLE_SIZE)
+    {
+      F4Vector sf_re;
+      F4Vector sf_im;
+      sf_re.f[0] = state_re;
+      sf_re.f[1] = state_re;
+      sf_re.f[2] = state_re;
+      sf_re.f[3] = state_re;
+      sf_im.f[0] = state_im;
+      sf_im.f[1] = state_im;
+      sf_im.f[2] = state_im;
+      sf_im.f[3] = state_im;
+
+      /*
+       * formula for complex multiplication:
+       *
+       * (state_re + i * state_im) * (inc_re + i * inc_im) =
+       *   state_re * inc_re - state_im * inc_im + i * (state_re * inc_im + state_im * inc_re)
+       */
+      F4Vector *new_im = reinterpret_cast<F4Vector *> (sin_begin + n);
+      F4Vector *new_re = reinterpret_cast<F4Vector *> (cos_begin + n);
+      for (int k = 0; k < TABLE_SIZE; k++)
+        {
+          new_re[k].v = _mm_add_ps (new_re[k].v, _mm_sub_ps (_mm_mul_ps (sf_re.v, incf_re[k].v),
+                                                             _mm_mul_ps (sf_im.v, incf_im[k].v)));
+          new_im[k].v = _mm_add_ps (new_im[k].v, _mm_add_ps (_mm_mul_ps (sf_re.v, incf_im[k].v),
+                                                 _mm_mul_ps (sf_im.v, incf_re[k].v)));
+        }
+
+      n += 4 * TABLE_SIZE;
+
+      /*
+       * (state_re + i * state_im) * (inc_re + i * inc_im) =
+       *   state_re * inc_re - state_im * inc_im + i * (state_re * inc_im + state_im * inc_re)
+       */
+      const double re = state_re * inc_re16 - state_im * inc_im16;
+      const double im = state_re * inc_im16 + state_im * inc_re16;
+      state_re = re;
+      state_im = im;
+
+      todo -= 4 * TABLE_SIZE;
+    }
+
+  // compute the remaining sin/cos values using the FPU
+  VectorSinParams rest_params = params;
+  rest_params.phase += n * phase_inc;
+  fast_vector_sincos_add (rest_params, sin_begin + n, sin_end, cos_begin + n);
+#else
+  fast_vector_sincos_add (params, sin_begin, sin_end, cos_begin);
+#endif
 }
 
 } // namespace SpectMorph
