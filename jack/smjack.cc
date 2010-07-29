fdee635ed9204a1368d6d699c2fb486fbc90fe80
Stefan Westerfeld <stefan@space.twc.de>
2010-07-29 11:18:07 +0200

    More JACK code; not yet working but already registering properly.

--- jack/smjack.cc
+++ jack/smjack.cc
@@ -15,7 +15,70 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <jack/jack.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+class JackSynth
+{
+protected:
+  double        jack_mix_freq;
+  jack_port_t  *input_port;
+  jack_port_t  *output_port;
+
+public:
+  void init (jack_client_t *client);
+  int  process (jack_nframes_t nframes);
+};
+
+int
+JackSynth::process (jack_nframes_t nframes)
+{
+  return 0;
+}
+
+int
+jack_process (jack_nframes_t nframes, void *arg)
+{
+  JackSynth *instance = reinterpret_cast<JackSynth *> (arg);
+  return instance->process (nframes);
+}
+
+void
+JackSynth::init (jack_client_t *client)
+{
+  jack_set_process_callback (client, jack_process, this);
+
+  jack_mix_freq = jack_get_sample_rate (client);
+
+  input_port = jack_port_register (client, "midi_in", JACK_DEFAULT_MIDI_TYPE, JackPortIsInput, 0);
+  output_port = jack_port_register (client, "audio_out", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+
+  if (jack_activate (client))
+    {
+      fprintf (stderr, "cannot activate client");
+      exit (1);
+    }
+}
+
 int
 main()
 {
+  jack_client_t *client;
+  client = jack_client_open ("smjack", JackNullOption, NULL);
+
+  if (!client)
+    {
+      fprintf (stderr, "unable to connect to jack server\n");
+      exit (1);
+    }
+
+  JackSynth synth;
+  synth.init (client);
+
+  while (1)
+    {
+      sleep (1);
+    }
 }
