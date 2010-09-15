bd6b267b44884f52176997965d32fc9a644b6e25
Stefan Westerfeld <stefan@space.twc.de>
2010-09-15 10:51:21 +0200

    Added common initialization function to be used in main().

---
+++ lib/smmain.cc
@@ -0,0 +1,37 @@
+/* 
+ * Copyright (C) 2010 Stefan Westerfeld
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "smmain.hh"
+#include <bse/bsemain.h>
+
+namespace SpectMorph
+{
+
+void sm_init (int *argc_p, char ***argv_p)
+{
+  SfiInitValue values[] = {
+    { "stand-alone",            "true" }, /* no rcfiles etc. */
+    { "wave-chunk-padding",     NULL, 1, },
+    { "dcache-block-size",      NULL, 8192, },
+    { "dcache-cache-memory",    NULL, 5 * 1024 * 1024, },
+    { "load-core-plugins", "1" },
+    { NULL }
+  };
+  bse_init_inprocess (argc_p, argv_p, NULL, values);
+}
+
+}
