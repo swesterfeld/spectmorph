721574663ed1726795f17eacf3b7531874122792
Stefan Westerfeld <stefan@space.twc.de>
2010-10-12 09:43:39 +0200

    TESTS: added performance test for int_sincos

--- tests/testisincos.cc
+++ tests/testisincos.cc
@@ -20,16 +20,53 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <sys/time.h>
 
 #include <algorithm>
+#include <string>
 
 using namespace SpectMorph;
 using std::max;
+using std::string;
+
+static double acc[2] = { 0, 0 };
+
+static double
+gettime()
+{
+  timeval tv;
+  gettimeofday (&tv, 0);
+
+  return tv.tv_sec + tv.tv_usec / 1000000.0;
+}
+
+void
+perf_test()
+{
+  double clocks_per_sec = 2500.0 * 1000 * 1000;
+  double start = gettime();
+  static int CALLS = 1000 * 1000 * 1000;
+  for (int i = 0; i < CALLS; i++)
+    {
+      double is, ic;
+      int_sincos (i, &is, &ic);
+      acc[0] += is;
+      acc[1] += ic;
+    }
+  double end = gettime();
+  printf ("int_sincos: %f clocks/invocation\n", clocks_per_sec * (end - start) / CALLS);
+}
 
 int
 main (int argc, char **argv)
 {
   sm_init (&argc, &argv);
+
+  if (argc == 2 && string (argv[1]) == "perf")
+    {
+      perf_test();
+      return 0;
+    }
 
   double max_delta = 0;
   for (int i = 0; i < 256; i++)
