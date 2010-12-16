b0c31b03293e9a1a094de6bc2016aebe118c30af
Stefan Westerfeld <stefan@space.twc.de>
2010-12-16 16:01:49 +0100

    sminspector: moved Navigator class to its own file

---
+++ inspector/smnavigator.cc
@@ -0,0 +1,158 @@
+/*
+ * Copyright (C) 2010 Stefan Westerfeld
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "smnavigator.hh"
+#include "smmicroconf.hh"
+#include "smlivedecoder.hh"
+
+#include <assert.h>
+#include <bse/bseloader.h>
+
+using namespace SpectMorph;
+
+using std::string;
+
+Navigator::Navigator (const string& filename) :
+  dhandle (NULL)
+{
+  printf ("loading index: %s\n", filename.c_str());
+  MicroConf cfg (filename);
+
+  while (cfg.next())
+    {
+      string str;
+
+      if (cfg.command ("smset", str))
+        {
+          smset_combobox.append_text (str);
+        }
+      else if (cfg.command ("smset_dir", str))
+        {
+          smset_dir = str;
+        }
+      else
+        {
+          cfg.die_if_unknown();
+        }
+    }
+
+  set_title ("Instrument Navigator");
+  set_border_width (10);
+  set_default_size (300, 600);
+  index_vbox.set_spacing (10);
+  index_vbox.pack_start (smset_combobox, Gtk::PACK_SHRINK);
+  ref_tree_model = Gtk::ListStore::create (audio_chooser_cols);
+  tree_view.set_model (ref_tree_model);
+  tree_view_scrolled_window.add (tree_view);
+  index_vbox.pack_start (tree_view_scrolled_window);
+
+  tree_view.append_column ("Note", audio_chooser_cols.col_note);
+  tree_view.append_column ("Ch", audio_chooser_cols.col_channel);
+  tree_view.append_column ("Range", audio_chooser_cols.col_range);
+  tree_view.append_column ("Path", audio_chooser_cols.col_path);
+
+  ref_tree_selection = tree_view.get_selection();
+  ref_tree_selection->signal_changed().connect (sigc::mem_fun (*this, &Navigator::on_selection_changed));
+
+  source_button.set_label ("Source/Analysis");
+  index_vbox.pack_start (source_button, Gtk::PACK_SHRINK);
+  source_button.signal_toggled().connect (sigc::mem_fun (*this, &Navigator::on_selection_changed));
+  add (index_vbox);
+
+  show_position_button.set_label ("Show Position");
+  index_vbox.pack_start (show_position_button, Gtk::PACK_SHRINK);
+  show_position_button.signal_toggled().connect (sigc::mem_fun (*this, &Navigator::on_show_position_changed));
+
+  show();
+  show_all_children();
+  smset_combobox.signal_changed().connect (sigc::mem_fun (*this, &Navigator::on_combo_changed));
+}
+
+void
+Navigator::on_selection_changed()
+{
+  Gtk::TreeModel::iterator iter = ref_tree_selection->get_selected();
+  if (iter)
+    {
+      Gtk::TreeModel::Row row = *iter;
+      size_t i = row[audio_chooser_cols.col_wave_nr];
+      assert (i < wset.waves.size());
+
+      int channel = row[audio_chooser_cols.col_channel];
+
+      Audio *audio = wset.waves[i].audio;
+      assert (wset.waves[i].audio);
+
+      if (source_button.get_active())
+        {
+          LiveDecoder decoder (&wset);
+          decoder.retrigger (channel, audio->fundamental_freq, 127, audio->mix_freq);
+          decoded_samples.resize (audio->sample_count);
+          decoder.process (decoded_samples.size(), 0, 0, &decoded_samples[0]);
+          dhandle = gsl_data_handle_new_mem (1, 32, audio->mix_freq, 440, decoded_samples.size(), &decoded_samples[0], NULL);
+        }
+      else
+        {
+          dhandle = gsl_data_handle_new_mem (1, 32, audio->mix_freq, 440, audio->original_samples.size(), &audio->original_samples[0], NULL);
+        }
+      signal_dhandle_changed();
+    }
+}
+
+void
+Navigator::on_combo_changed()
+{
+  std::string file = smset_dir + "/" + smset_combobox.get_active_text().c_str();
+  printf ("loading %s...\n", file.c_str());
+  BseErrorType error = wset.load (file);
+  if (error)
+    {
+      fprintf (stderr, "sminspector: can't open input file: %s: %s\n", file.c_str(), bse_error_blurb (error));
+      exit (1);
+    }
+
+  ref_tree_model->clear();
+  for (size_t i = 0; i < wset.waves.size(); i++)
+    {
+      const WavSetWave& wave = wset.waves[i];
+
+      Gtk::TreeModel::Row row = *(ref_tree_model->append());
+      row[audio_chooser_cols.col_note] = wave.midi_note;
+      row[audio_chooser_cols.col_channel] = wave.channel;
+      row[audio_chooser_cols.col_range] = Birnet::string_printf ("%d..%d", wave.velocity_range_min, wave.velocity_range_max);
+      row[audio_chooser_cols.col_path] = wave.path;
+      row[audio_chooser_cols.col_wave_nr] = i;
+    }
+}
+
+void
+Navigator::on_show_position_changed()
+{
+  signal_show_position_changed();
+}
+
+GslDataHandle *
+Navigator::get_dhandle()
+{
+  return dhandle;
+}
+
+bool
+Navigator::get_show_position()
+{
+  return show_position_button.get_active();
+}
---
+++ inspector/smnavigator.hh
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2010 Stefan Westerfeld
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SPECTMORPH_NAVIGATOR_HH
+#define SPECTMORPH_NAVIGATOR_HH
+
+#include <gtkmm.h>
+
+#include "smwavset.hh"
+
+namespace SpectMorph {
+
+class Navigator : public Gtk::Window
+{
+  SpectMorph::WavSet    wset;
+
+  std::string           smset_dir;
+  Gtk::ComboBoxText     smset_combobox;
+  Gtk::VBox             index_vbox;
+  Gtk::ToggleButton     show_position_button;
+  std::vector<float>    decoded_samples;
+
+  struct ModelColumns : public Gtk::TreeModel::ColumnRecord
+  {
+    ModelColumns()
+    {
+      add (col_note);
+      add (col_channel);
+      add (col_range);
+      add (col_path);
+      add (col_wave_nr);
+    }
+    Gtk::TreeModelColumn<int>           col_note;
+    Gtk::TreeModelColumn<int>           col_channel;
+    Gtk::TreeModelColumn<Glib::ustring> col_range;
+    Gtk::TreeModelColumn<Glib::ustring> col_path;
+    Gtk::TreeModelColumn<int>           col_wave_nr;
+  };
+
+  ModelColumns audio_chooser_cols;
+  Glib::RefPtr<Gtk::ListStore>       ref_tree_model;
+  Glib::RefPtr<Gtk::TreeSelection>   ref_tree_selection;
+  Gtk::ScrolledWindow                tree_view_scrolled_window;
+  Gtk::TreeView                      tree_view;
+
+  Gtk::ToggleButton                  source_button;
+
+  GslDataHandle                     *dhandle;
+
+public:
+  sigc::signal<void> signal_dhandle_changed;
+  sigc::signal<void> signal_show_position_changed;
+
+  Navigator (const std::string& filename);
+
+  void on_combo_changed();
+  void on_selection_changed();
+  void on_show_position_changed();
+
+  GslDataHandle *get_dhandle();
+  bool           get_show_position();
+};
+
+}
+
+#endif
