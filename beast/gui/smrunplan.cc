d103d47dc552e2b052c481b517e4cd9c75821e70
Stefan Westerfeld <stefan@space.twc.de>
2011-03-11 20:21:56 +0100

    smrunplan: started implementing plan running

--- beast/gui/smrunplan.cc
+++ beast/gui/smrunplan.cc
@@ -15,9 +15,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "smmorphplan.hh"
+#include "smmorphoutput.hh"
 #include "smmain.hh"
 
 using namespace SpectMorph;
+
+using std::vector;
 
 int
 main (int argc, char **argv)
@@ -39,4 +42,27 @@ main (int argc, char **argv)
     }
   plan.load (in);
   printf ("\n\nSUCCESS: plan loaded, %zd operators found.\n", plan.operators().size());
+
+  const vector<MorphOperator *>& ops = plan.operators();
+
+  MorphOutput *output = NULL;
+  for (vector<MorphOperator *>::const_iterator oi = ops.begin(); oi != ops.end(); oi++)
+    {
+      MorphOperator *op = *oi;
+      if (!strcmp (op->type(), "SpectMorph::MorphOutput"))
+        output = dynamic_cast<MorphOutput *> (op);
+    }
+  if (!output)
+    {
+      printf ("failed to find output\n");
+      exit (1);
+    }
+  printf ("Output: %s\n", output->name().c_str());
+  MorphOperator *out_op = output->channel_op (0);
+  if (!out_op)
+    {
+      printf ("no output 0 defined\n");
+      exit (1);
+    }
+  printf ("Output[0] = %s\n", out_op->name().c_str());
 }
