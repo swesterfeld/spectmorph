19120943bc85261c3ffd5bf9ac347e2aba25bc98
Stefan Westerfeld <stefan@space.twc.de>
2011-03-28 12:07:55 +0200

    TESTS: added simple plan update performance test

---
+++ tests/testparamupdate.cc
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2010-2011 Stefan Westerfeld
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "smmorphplan.hh"
+#include "smmorphplanvoice.hh"
+#include "smmorphoutputmodule.hh"
+#include "smmain.hh"
+
+#include <sys/time.h>
+
+using namespace SpectMorph;
+
+static double
+gettime()
+{
+  timeval tv;
+  gettimeofday (&tv, 0);
+
+  return tv.tv_sec + tv.tv_usec / 1000000.0;
+}
+
+int
+main (int argc, char **argv)
+{
+  sm_init (&argc, &argv);
+  if (argc != 2)
+    {
+      printf ("usage: %s <plan>\n", argv[0]);
+      exit (1);
+    }
+
+  MorphPlanPtr plan = new MorphPlan();
+  GenericIn *in = StdioIn::open (argv[1]);
+  if (!in)
+    {
+      g_printerr ("Error opening '%s'.\n", argv[1]);
+      exit (1);
+    }
+  plan->load (in);
+  delete in;
+
+  fprintf (stderr, "SUCCESS: plan loaded, %zd operators found.\n", plan->operators().size());
+
+  MorphPlanVoice voice (plan);
+
+  size_t runs = 1000000;
+
+  double start = gettime();
+  for (size_t i = 0; i < runs; i++)
+    voice.update (plan);
+  double end = gettime();
+
+  printf ("update: %f updates per ms\n", 1 / ((end - start) * 1000 / runs));
+}
