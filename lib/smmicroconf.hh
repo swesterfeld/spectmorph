7af948138faff0bc9ebe29681fa37ffa4bbfb248
Stefan Westerfeld <stefan@space.twc.de>
2011-04-01 17:46:21 +0200

    LIB: support config entries with more arguments than two in MicroConf

--- lib/smmicroconf.hh
+++ lib/smmicroconf.hh
@@ -62,6 +62,37 @@ public:
       return false;
     return convert (tokens[1], arg1) && convert (tokens[2], arg2);
   }
+  template<class T1, class T2, class T3>
+  bool command (const std::string& cmd, T1& arg1, T2& arg2, T3& arg3)
+  {
+    if (tokenizer_error || tokens.size() != 4 || cmd != tokens[0])
+      return false;
+    return convert (tokens[1], arg1) && convert (tokens[2], arg2) && convert (tokens[3], arg3);
+  }
+  template<class T1, class T2, class T3, class T4>
+  bool command (const std::string& cmd, T1& arg1, T2& arg2, T3& arg3, T4& arg4)
+  {
+    if (tokenizer_error || tokens.size() != 5 || cmd != tokens[0])
+      return false;
+    return convert (tokens[1], arg1) && convert (tokens[2], arg2) && convert (tokens[3], arg3)
+      &&   convert (tokens[4], arg4);
+  }
+  template<class T1, class T2, class T3, class T4, class T5>
+  bool command (const std::string& cmd, T1& arg1, T2& arg2, T3& arg3, T4& arg4, T5& arg5)
+  {
+    if (tokenizer_error || tokens.size() != 6 || cmd != tokens[0])
+      return false;
+    return convert (tokens[1], arg1) && convert (tokens[2], arg2) && convert (tokens[3], arg3)
+      &&   convert (tokens[4], arg4) && convert (tokens[5], arg5);
+  }
+  template<class T1, class T2, class T3, class T4, class T5, class T6>
+  bool command (const std::string& cmd, T1& arg1, T2& arg2, T3& arg3, T4& arg4, T5& arg5, T6& arg6)
+  {
+    if (tokenizer_error || tokens.size() != 7 || cmd != tokens[0])
+      return false;
+    return convert (tokens[1], arg1) && convert (tokens[2], arg2) && convert (tokens[3], arg3)
+      &&   convert (tokens[4], arg4) && convert (tokens[5], arg5) && convert (tokens[6], arg6);
+  }
 };
 
 }
