8504ebac985eea1fd6184aabb2478752e6e60625
Stefan Westerfeld <stefan@space.twc.de>
2011-04-12 17:11:34 +0200

    smsampleedit: started impleminting sample editor

--- tools/smsampleedit.cc
+++ tools/smsampleedit.cc
@@ -18,12 +18,15 @@
 #include <gtkmm.h>
 #include <assert.h>
 #include <sys/time.h>
+#include <bse/gsldatautils.h>
+#include <bse/bseloader.h>
 
 #include <vector>
 #include <string>
 
 #include "smmain.hh"
 #include "smsampleview.hh"
+#include "smzoomcontroller.hh"
 
 using namespace SpectMorph;
 
@@ -32,16 +35,99 @@ using std::vector;
 
 class MainWindow : public Gtk::Window
 {
-  SampleView view;
+  Gtk::ScrolledWindow scrolled_win;
+  Gtk::HBox           button_hbox;
+  Gtk::VBox           vbox;
+  SampleView          sample_view;
+  Audio               audio;
+  ZoomController      zoom_controller;
+  Gtk::ToggleButton   edit_clip_start;
+  Gtk::ToggleButton   edit_clip_end;
+  Gtk::Label          time_label;
 
 public:
   MainWindow();
+
+  void on_zoom_changed();
+  void on_mouse_time_changed (int time);
+  void load (GslDataHandle *dhandle);
 };
 
-MainWindow::MainWindow()
+MainWindow::MainWindow() :
+  zoom_controller (1, 5000, 10, 5000)
 {
-  add (view);
+  scrolled_win.add (sample_view);
+
+  vbox.pack_start (scrolled_win);
+  vbox.pack_start (zoom_controller, Gtk::PACK_SHRINK);
+  vbox.pack_start (button_hbox, Gtk::PACK_SHRINK);
+
+  zoom_controller.signal_zoom_changed.connect (sigc::mem_fun (*this, &MainWindow::on_zoom_changed));
+  sample_view.signal_mouse_time_changed.connect (sigc::mem_fun (*this, &MainWindow::on_mouse_time_changed));
+  on_mouse_time_changed (0); // init label
+
+  edit_clip_start.set_label ("Edit Clip Start");
+  edit_clip_end.set_label ("Edit Clip End");
+  button_hbox.pack_start (time_label);
+  button_hbox.pack_start (edit_clip_start);
+  button_hbox.pack_start (edit_clip_end);
+  add (vbox);
   show_all_children();
+}
+
+void
+MainWindow::load (GslDataHandle *dhandle)
+{
+  gsl_data_handle_open (dhandle);
+  audio.mix_freq = gsl_data_handle_mix_freq (dhandle);
+  sample_view.load (dhandle, &audio);
+  gsl_data_handle_close (dhandle);
+}
+
+void
+MainWindow::on_zoom_changed()
+{
+  sample_view.set_zoom (zoom_controller.get_hzoom(), zoom_controller.get_vzoom());
+}
+
+void
+MainWindow::on_mouse_time_changed (int time)
+{
+  int ms = time % 1000;
+  time /= 1000;
+  int s = time % 60;
+  time /= 60;
+  int m = time;
+  time_label.set_label (Birnet::string_printf ("Time: %02d:%02d:%03d ms", m, s, ms));
+}
+
+GslDataHandle*
+dhandle_from_file (const string& filename)
+{
+  /* open input */
+  BseErrorType error;
+
+  BseWaveFileInfo *wave_file_info = bse_wave_file_info_load (filename.c_str(), &error);
+  if (!wave_file_info)
+    {
+      fprintf (stderr, "SpectMorph::WavLoader: can't open the input file %s: %s\n", filename.c_str(), bse_error_blurb (error));
+      return NULL;
+    }
+
+  BseWaveDsc *waveDsc = bse_wave_dsc_load (wave_file_info, 0, FALSE, &error);
+  if (!waveDsc)
+    {
+      fprintf (stderr, "SpectMorph::WavLoader: can't open the input file %s: %s\n", filename.c_str(), bse_error_blurb (error));
+      return NULL;
+    }
+
+  GslDataHandle *dhandle = bse_wave_handle_create (waveDsc, 0, &error);
+  if (!dhandle)
+    {
+      fprintf (stderr, "SpectMorph::WavLoader: can't open the input file %s: %s\n", filename.c_str(), bse_error_blurb (error));
+      return NULL;
+    }
+  return dhandle;
 }
 
 int
@@ -51,13 +137,14 @@ main (int argc, char **argv)
 
   Gtk::Main kit (argc, argv);
 
-  if (argc != 1)
+  if (argc != 2)
     {
-      printf ("usage: %s\n", argv[0]);
+      printf ("usage: %s <wave>\n", argv[0]);
       exit (1);
     }
 
   MainWindow main_window;
+  main_window.load (dhandle_from_file (argv[1]));
   Gtk::Main::run (main_window);
 
   return 0;
