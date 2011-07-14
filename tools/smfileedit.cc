3e17063fdc8e4214fc4e7b46638e396a665b34dd
Stefan Westerfeld <stefan@space.twc.de>
2011-07-14 10:34:00 +0200

    smfileedit: added blob support

--- tools/smfileedit.cc
+++ tools/smfileedit.cc
@@ -17,31 +17,20 @@
 
 #include "sminfile.hh"
 #include "smoutfile.hh"
+#include "smmemout.hh"
 #include "smmain.hh"
+
+#include <stdlib.h>
 
 #define PROG_NAME "smfileedit"
 
 using namespace SpectMorph;
 
-int
-main (int argc, char **argv)
+using std::vector;
+
+void
+process_file (InFile& ifile, OutFile& ofile)
 {
-  sm_init (&argc, &argv);
-
-  if (argc != 3)
-    {
-      printf ("usage: " PROG_NAME " <infile> <outfile> [<command>...]\n");
-      return 1;
-    }
-  GenericIn *in = StdioIn::open (argv[1]);
-  if (!in)
-    {
-      fprintf (stderr, PROG_NAME ": error opening input\n");
-      return 1;
-    }
-  InFile ifile (in);
-  OutFile ofile (argv[2], ifile.file_type(), ifile.file_version());
-
   while (ifile.event() != InFile::END_OF_FILE)
     {
      if (ifile.event() == InFile::BEGIN_SECTION)
@@ -74,9 +63,49 @@ main (int argc, char **argv)
         }
       else if (ifile.event() == InFile::BLOB)
         {
-          printf ("<no blob support>\n");
+          GenericIn *blob_in = ifile.open_blob();
+          if (!blob_in)
+            {
+              fprintf (stderr, PROG_NAME ": error opening input\n");
+              exit (1);
+            }
+          InFile blob_ifile (blob_in);
+
+          vector<unsigned char> blob_data;
+          MemOut                blob_mo (&blob_data);
+
+          {
+            OutFile blob_ofile (&blob_mo, blob_ifile.file_type(), blob_ifile.file_version());
+            process_file (blob_ifile, blob_ofile);
+            // blob_ofile destructor run here
+          }
+
+          ofile.write_blob ("data", &blob_data[0], blob_data.size());
+          delete blob_in;
         }
       ifile.next_event();
     }
+}
+
+int
+main (int argc, char **argv)
+{
+  sm_init (&argc, &argv);
+
+  if (argc != 3)
+    {
+      printf ("usage: " PROG_NAME " <infile> <outfile> [<command>...]\n");
+      return 1;
+    }
+  GenericIn *in = StdioIn::open (argv[1]);
+  if (!in)
+    {
+      fprintf (stderr, PROG_NAME ": error opening input\n");
+      return 1;
+    }
+  InFile ifile (in);
+  OutFile ofile (argv[2], ifile.file_type(), ifile.file_version());
+
+  process_file (ifile, ofile);
   delete in;
 }
