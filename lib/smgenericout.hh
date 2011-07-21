ca245cf76f7950dc7014b9d07155fb17b7a6eede
Stefan Westerfeld <stefan@space.twc.de>
2011-07-21 16:01:19 +0200

    DOCS: API docs update

--- lib/smgenericout.hh
+++ lib/smgenericout.hh
@@ -23,12 +23,31 @@
 namespace SpectMorph
 {
 
+/**
+ * \brief Generic Output Stream
+ *
+ * This class is the abstract base class for different (binary) output streams
+ * like "to memory", "to file", ...
+ */
 class GenericOut
 {
 public:
   virtual ~GenericOut();
 
+  /**
+   * Write one character into the output stream.
+   *
+   * \param c character to be written
+   * \returns the character written, or EOF on error.
+   */
   virtual int put_byte (int c) = 0;     // like fputc
+  /**
+   * Write a block of data into the stream.
+   *
+   * \param ptr pointer to the data to be written
+   * \param size number of bytes to be written
+   * \returns the number of bytes successfully written (can be 0)
+   */
   virtual int write (const void *ptr, size_t size) = 0;
 };
 
