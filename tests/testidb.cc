97aaf43d472e160d606d1bfcf66842860d6ff6f7
Stefan Westerfeld <stefan@space.twc.de>
2016-06-16 23:34:29 +0200

    TESTS: test sm_factor2idb with extremely small numbers

--- tests/testidb.cc
+++ tests/testidb.cc
@@ -18,7 +18,7 @@ main (int argc, char **argv)
 {
   sm_init (&argc, &argv);
 
-  double emin = 0, emax = 0, econv = 0;
+  double emin = 0, emax = 0, econv = 0, esmall = 0;
   for (size_t i = 0; i < 65536; i++)
     {
       econv = max (econv, (sm_idb2factor (i) - sm_idb2factor_slow (i)) / sm_idb2factor_slow (i));
@@ -40,7 +40,24 @@ main (int argc, char **argv)
   const double bound = 0.0009;
   printf ("representation error%%: [%.6f, %.6f] bound %.6f\n", emin * 100, emax * 100, bound * 100);
 
+  const double small_bound = 1.1e-25;
+  for (int exp = -1;; exp--)
+    {
+      double small = pow (10, exp);
+
+      int16_t idb = sm_factor2idb (small);
+      double xsmall = sm_idb2factor_slow (idb);
+      double error = fabs (small - xsmall);
+
+      esmall = max (error, esmall);
+
+      if (small == 0)  // break if exp was so small that 10^exp == 0
+        break;
+    }
+  printf ("esmall: %.7g bound %.7g\n", esmall, small_bound);
+
   assert (econv < conv_bound);
   assert (-emin < bound);
   assert (emax  < bound);
+  assert (esmall < small_bound);
 }
