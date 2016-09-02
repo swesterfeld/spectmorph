495b83c48da2a1a353d3b44d09c9782555d3b04c
Stefan Westerfeld <stefan@space.twc.de>
2016-09-02 12:07:03 +0200

    VST: started to implement vst plugin

---
+++ vst/smvstplugin.cc
@@ -0,0 +1,457 @@
+/*
+ *  VST Plugin is under GPL 2 or later, since it is based on Vestige (GPL) and amsynth_vst (GPL)
+ *
+ *  Copyright (c) 2008-2015 Nick Dowell
+ *  Copyright (c) 2016 Stefan Westerfeld
+ */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <cassert>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+
+#include "vestige/aeffectx.h"
+#include "smutils.hh"
+#include "smmorphplan.hh"
+#include "smmorphplansynth.hh"
+#include "smmidisynth.hh"
+#include "smmain.hh"
+#include "smmorphplanwindow.hh"
+
+#include <QMutex>
+#include <QWindow>
+#include <QPushButton>
+#include <QApplication>
+
+// from http://www.asseca.org/vst-24-specs/index.html
+#define effGetParamLabel        6
+#define effGetParamDisplay      7
+#define effGetChunk             23
+#define effSetChunk             24
+#define effCanBeAutomated       26
+#define effGetOutputProperties  34
+#define effGetTailSize          52
+#define effGetMidiKeyName       66
+#define effBeginLoadBank        75
+#define effFlagsProgramChunks   (1 << 5)
+
+#define DEBUG 1
+
+using namespace SpectMorph;
+
+using std::string;
+
+static FILE *debug_file = NULL;
+QMutex       debug_mutex;
+
+static void
+debug (const char *fmt, ...)
+{
+  if (DEBUG)
+    {
+      QMutexLocker locker (&debug_mutex);
+
+      if (!debug_file)
+        debug_file = fopen ("/tmp/smvstplugin.log", "w");
+
+      va_list ap;
+
+      va_start (ap, fmt);
+      fprintf (debug_file, "%s", string_vprintf (fmt, ap).c_str());
+      va_end (ap);
+      fflush (debug_file);
+    }
+}
+
+static bool
+is_note_on (const VstMidiEvent *event)
+{
+  if ((event->midiData[0] & 0xf0) == 0x90)
+    {
+      if (event->midiData[2] != 0) /* note on with velocity 0 => note off */
+        return true;
+    }
+  return false;
+}
+
+static bool
+is_note_off (const VstMidiEvent *event)
+{
+  if ((event->midiData[0] & 0xf0) == 0x90)
+    {
+      if (event->midiData[2] == 0) /* note on with velocity 0 => note off */
+        return true;
+    }
+  else if ((event->midiData[0] & 0xf0) == 0x80)
+    {
+      return true;
+    }
+  return false;
+}
+
+struct ERect
+{
+	short top;
+	short left;
+	short bottom;
+	short right;
+};
+
+static char hostProductString[64] = "";
+
+class VstUI //: public QObject
+{
+  //Q_OBJECT
+
+  ERect             rectangle;
+  MorphPlanWindow  *widget;
+  MorphPlanPtr      morph_plan;
+public:
+  VstUI (const string& filename) :
+    morph_plan (new MorphPlan())
+  {
+    GenericIn *in = StdioIn::open (filename);
+    if (!in)
+      {
+        g_printerr ("Error opening '%s'.\n", filename.c_str());
+        exit (1);
+      }
+    morph_plan->load (in);
+    delete in;
+  }
+
+  bool
+  open (WId win_id)
+  {
+    widget = new MorphPlanWindow (morph_plan, "!title!");
+#if 0
+    QObject::connect(widget, &QPushButton::clicked, [=](bool b) {
+      debug ("clicked\n");
+    });
+#endif
+    //connect (widget, SIGNAL(clicked()), this, SLOT (doit()));
+    widget->winId();
+    widget->windowHandle()->setParent (QWindow::fromWinId (win_id));
+    widget->show();
+    rectangle.top = 0;
+    rectangle.left = 0;
+    rectangle.bottom = widget->height();
+    rectangle.right = widget->width();
+
+    return true;
+  }
+  bool
+  getRect (ERect** rect)
+  {
+    *rect = &rectangle;
+
+    return true;
+  }
+  void
+  close()
+  {
+    delete widget;
+  }
+  void
+  idle()
+  {
+    QApplication::processEvents();
+  }
+};
+
+struct Plugin
+{
+  Plugin(audioMasterCallback master) :
+    plan (new MorphPlan()),
+    morph_plan_synth (48000), // FIXME
+    midi_synth (morph_plan_synth, 48000, 64), // FIXME
+    ui (new VstUI ("/home/stefan/lv2.smplan"))
+  {
+    audioMaster = master;
+
+    std::string filename = "/home/stefan/lv2.smplan";
+    GenericIn *in = StdioIn::open (filename);
+    if (!in)
+      {
+        g_printerr ("Error opening '%s'.\n", filename.c_str());
+        exit (1);
+      }
+    plan->load (in);
+    delete in;
+
+    morph_plan_synth.update_plan (plan);
+  }
+
+  ~Plugin()
+  {
+    delete ui;
+    ui = nullptr;
+  }
+
+  audioMasterCallback audioMaster;
+  MorphPlanPtr        plan;
+  MorphPlanSynth      morph_plan_synth;
+  MidiSynth           midi_synth;
+  VstUI              *ui;
+};
+
+static intptr_t dispatcher(AEffect *effect, int opcode, int index, intptr_t val, void *ptr, float f)
+{
+	Plugin *plugin = (Plugin *)effect->ptr3;
+
+	switch (opcode) {
+		case effOpen:
+			return 0;
+
+		case effClose:
+			delete plugin;
+			memset(effect, 0, sizeof(AEffect));
+			free(effect);
+			return 0;
+
+		case effSetProgram:
+		case effGetProgram:
+		case effGetProgramName:
+			return 0;
+
+#if 0 // FIXME
+		case effGetParamLabel:
+			// FIXME plugin->synthesizer->getParameterLabel((Param)index, (char *)ptr, 32);
+			return 0;
+
+		case effGetParamDisplay:
+			// FIXME plugin->synthesizer->getParameterDisplay((Param)index, (char *)ptr, 32);
+			return 0;
+
+		case effGetParamName:
+			// FIXME plugin->synthesizer->getParameterName((Param)index, (char *)ptr, 32);
+			return 0;
+
+#endif
+		case effSetSampleRate:
+			debug ("fake set sample rate %f\n", f); //// FIXME plugin->synthesizer->setSampleRate(f);
+			return 0;
+
+		case effSetBlockSize:
+		case effMainsChanged:
+			return 0;
+
+    case effEditGetRect:
+      plugin->ui->getRect ((ERect **) ptr);
+      return 1;
+
+    case effEditOpen:
+      plugin->ui->open((WId)(uintptr_t)ptr);
+      return 1;
+
+    case effEditClose:
+      plugin->ui->close();
+      return 0;
+
+    case effEditIdle:
+      plugin->ui->idle();
+      return 0;
+
+#if 0 // FIXME
+		case effGetChunk:
+			// FIXME return plugin->synthesizer->saveState((char **)ptr);
+
+		case effSetChunk:
+			// FIXME plugin->synthesizer->loadState((char *)ptr);
+			return 0;
+#endif
+
+		case effProcessEvents: {
+			VstEvents *events = (VstEvents *)ptr;
+
+			for (int32_t i=0; i<events->numEvents; i++)
+                          {
+                            VstMidiEvent *event = (VstMidiEvent *)events->events[i];
+                            if (event->type != kVstMidiType)
+                              continue;
+
+                            debug ("EV: %x %d %d\n", event->midiData[0], event->midiData[1], event->midiData[2]);
+                            if (is_note_on (event))
+                              {
+                                plugin->midi_synth.process_note_on (event->midiData[1], event->midiData[2]);
+                              }
+                            else if (is_note_off (event))
+                              {
+                                plugin->midi_synth.process_note_off (event->midiData[1]);
+                              }
+			  }
+
+			return 1;
+#if 0 // FIXME
+			VstEvents *events = (VstEvents *)ptr;
+
+			assert(plugin->midiEvents.empty());
+
+			memset(plugin->midiBuffer, 0, 4096);
+			unsigned char *buffer = plugin->midiBuffer;
+			
+			for (int32_t i=0; i<events->numEvents; i++) {
+				VstMidiEvent *event = (VstMidiEvent *)events->events[i];
+				if (event->type != kVstMidiType) {
+					continue;
+				}
+
+				memcpy(buffer, event->midiData, 4);
+
+				amsynth_midi_event_t midi_event;
+				memset(&midi_event, 0, sizeof(midi_event));
+				midi_event.offset_frames = event->deltaFrames;
+				midi_event.buffer = buffer;
+				midi_event.length = 4;
+				plugin->midiEvents.push_back(midi_event);
+
+				buffer += event->byteSize;
+
+				assert(buffer < plugin->midiBuffer + 4096);
+#endif
+		}
+
+		case effCanBeAutomated:
+		case effGetOutputProperties:
+			return 0;
+
+		case effGetPlugCategory:
+			return kPlugCategSynth;
+		case effGetEffectName:
+			strcpy((char *)ptr, "SpectMorph");
+			return 1;
+		case effGetVendorString:
+			strcpy((char *)ptr, "Stefan Westerfeld");
+			return 1;
+#if 0
+		case effGetProductString:
+			strcpy((char *)ptr, "amsynth");
+			return 1;
+#endif
+		case effGetVendorVersion:
+			return 0;
+
+		case effCanDo:
+			if (strcmp("receiveVstMidiEvent", (char *)ptr) == 0 ||
+				false) return 1;
+			if (strcmp("midiKeyBasedInstrumentControl", (char *)ptr) == 0 ||
+				strcmp("midiSingleNoteTuningChange", (char *)ptr) == 0 ||
+				strcmp("receiveVstSysexEvent", (char *)ptr) == 0 ||
+				strcmp("sendVstMidiEvent", (char *)ptr) == 0 ||
+				false) return 0;
+			debug("unhandled canDo: %s\n", (char *)ptr);
+			return 0;
+
+		case effGetTailSize:
+		case effIdle:
+		case effGetParameterProperties:
+			return 0;
+
+		case effGetVstVersion:
+			return 2400;
+
+		case effGetMidiKeyName:
+		case effStartProcess:
+		case effStopProcess:
+		case effBeginSetProgram:
+		case effEndSetProgram:
+		case effBeginLoadBank:
+			return 0;
+
+		default:
+			debug ("[smvstplugin] unhandled VST opcode: %d\n", opcode);
+			return 0;
+	}
+}
+
+static void process(AEffect *effect, float **inputs, float **outputs, int numSampleFrames)
+{
+  Plugin *plugin = (Plugin *)effect->ptr3;
+  debug ("!missing process\n");
+#if 0 // FIXME
+  std::vector<amsynth_midi_cc_t> midi_out;
+  plugin->synthesizer->process(numSampleFrames, plugin->midiEvents, midi_out, outputs[0], outputs[1]);
+  plugin->midiEvents.clear();
+#endif
+}
+
+static void processReplacing(AEffect *effect, float **inputs, float **outputs, int numSampleFrames)
+{
+  Plugin *plugin = (Plugin *)effect->ptr3;
+
+  plugin->midi_synth.process_audio (outputs[0], numSampleFrames);
+  std::copy_n (outputs[0], numSampleFrames, outputs[1]);
+}
+
+static void setParameter(AEffect *effect, int i, float f)
+{
+  Plugin *plugin = (Plugin *)effect->ptr3;
+  debug ("!missing set parameter\n");
+
+  // FIXME plugin->ynthesizer->setNormalizedParameterValue((Param) i, f);
+}
+
+static float getParameter(AEffect *effect, int i)
+{
+  Plugin *plugin = (Plugin *)effect->ptr3;
+  debug ("!missing get parameter\n");
+
+  // FIXME return plugin->synthesizer->getNormalizedParameterValue((Param) i);
+}
+
+extern "C" AEffect * VSTPluginMain(audioMasterCallback audioMaster)
+{
+  debug ("VSTPluginMain called\n");
+  if (audioMaster)
+    {
+      audioMaster (NULL, audioMasterGetProductString, 0, 0, hostProductString, 0.0f);
+    }
+
+  if (!sm_init_done())
+    sm_init_plugin();
+
+  if (qApp)
+    {
+      debug ("... (have qapp) ...\n");
+    }
+  else
+    {
+      printf ("...  (creating qapp) ...\n");
+      static int argc = 0;
+      new QApplication(argc, NULL, true);
+    }
+
+  AEffect *effect = (AEffect *)calloc(1, sizeof(AEffect));
+  effect->magic = kEffectMagic;
+  effect->dispatcher = dispatcher;
+  effect->process = process;
+  effect->setParameter = setParameter;
+  effect->getParameter = getParameter;
+  effect->numPrograms = 0;
+  effect->numParams = 0; // FIXME kAmsynthParameterCount;
+  effect->numInputs = 0;
+  effect->numOutputs = 2;
+  effect->flags = effFlagsCanReplacing | effFlagsIsSynth | effFlagsProgramChunks | effFlagsHasEditor;
+
+  // Do no use the ->user pointer because ardour clobbers it
+  effect->ptr3 = new Plugin(audioMaster);
+  effect->uniqueID = CCONST ('s', 'm', 'r', 'p');
+  effect->processReplacing = processReplacing;
+
+  debug ("VSTPluginMain done => return %p\n", effect);
+  return effect;
+}
+
+#if 0
+// this is required because GCC throws an error if we declare a non-standard function named 'main'
+extern "C" __attribute__ ((visibility("default"))) AEffect * main_plugin(audioMasterCallback audioMaster) asm ("main");
+
+extern "C" __attribute__ ((visibility("default"))) AEffect * main_plugin(audioMasterCallback audioMaster)
+{
+	return VSTPluginMain (audioMaster);
+}
+#endif
---
+++ vst/vestige/aeffectx.h
@@ -0,0 +1,318 @@
+/*
+ * aeffectx.h - simple header to allow VeSTige compilation and eventually work
+ *
+ * Copyright (c) 2006 Javier Serrano Polo <jasp00/at/users.sourceforge.net>
+ *
+ * This file is part of LMMS - http://lmms.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+
+#ifndef AEFFECTX_H
+#define AEFFECTX_H
+
+#include <stdint.h>
+
+#define CCONST(a, b, c, d)( ( ( (int) a ) << 24 ) |		\
+				( ( (int) b ) << 16 ) |		\
+				( ( (int) c ) << 8 ) |		\
+				( ( (int) d ) << 0 ) )
+
+const int audioMasterAutomate = 0;
+const int audioMasterVersion = 1;
+const int audioMasterCurrentId = 2;
+const int audioMasterIdle = 3;
+const int audioMasterPinConnected = 4;
+// unsupported? 5
+const int audioMasterWantMidi = 6;
+const int audioMasterGetTime = 7;
+const int audioMasterProcessEvents = 8;
+const int audioMasterSetTime = 9;
+const int audioMasterTempoAt = 10;
+const int audioMasterGetNumAutomatableParameters = 11;
+const int audioMasterGetParameterQuantization = 12;
+const int audioMasterIOChanged = 13;
+const int audioMasterNeedIdle = 14;
+const int audioMasterSizeWindow = 15;
+const int audioMasterGetSampleRate = 16;
+const int audioMasterGetBlockSize = 17;
+const int audioMasterGetInputLatency = 18;
+const int audioMasterGetOutputLatency = 19;
+const int audioMasterGetPreviousPlug = 20;
+const int audioMasterGetNextPlug = 21;
+const int audioMasterWillReplaceOrAccumulate = 22;
+const int audioMasterGetCurrentProcessLevel = 23;
+const int audioMasterGetAutomationState = 24;
+const int audioMasterOfflineStart = 25;
+const int audioMasterOfflineRead = 26;
+const int audioMasterOfflineWrite = 27;
+const int audioMasterOfflineGetCurrentPass = 28;
+const int audioMasterOfflineGetCurrentMetaPass = 29;
+const int audioMasterSetOutputSampleRate = 30;
+// unsupported? 31
+const int audioMasterGetSpeakerArrangement = 31; // deprecated in 2.4?
+const int audioMasterGetVendorString = 32;
+const int audioMasterGetProductString = 33;
+const int audioMasterGetVendorVersion = 34;
+const int audioMasterVendorSpecific = 35;
+const int audioMasterSetIcon = 36;
+const int audioMasterCanDo = 37;
+const int audioMasterGetLanguage = 38;
+const int audioMasterOpenWindow = 39;
+const int audioMasterCloseWindow = 40;
+const int audioMasterGetDirectory = 41;
+const int audioMasterUpdateDisplay = 42;
+const int audioMasterBeginEdit = 43;
+const int audioMasterEndEdit = 44;
+const int audioMasterOpenFileSelector = 45;
+const int audioMasterCloseFileSelector = 46; // currently unused
+const int audioMasterEditFile = 47; // currently unused
+const int audioMasterGetChunkFile = 48; // currently unused
+const int audioMasterGetInputSpeakerArrangement = 49; // currently unused
+
+const int effFlagsHasEditor = 1;
+const int effFlagsCanReplacing = 1 << 4; // very likely
+const int effFlagsIsSynth = 1 << 8; // currently unused
+
+const int effOpen = 0;
+const int effClose = 1; // currently unused
+const int effSetProgram = 2; // currently unused
+const int effGetProgram = 3; // currently unused
+const int effGetProgramName = 5; // currently unused
+const int effGetParamName = 8; // currently unused
+const int effSetSampleRate = 10;
+const int effSetBlockSize = 11;
+const int effMainsChanged = 12;
+const int effEditGetRect = 13;
+const int effEditOpen = 14;
+const int effEditClose = 15;
+const int effEditIdle = 19;
+const int effEditTop = 20;
+const int effProcessEvents = 25;
+// the next one from http://asseca.com/vst-24-specs/index.html
+const int effGetPlugCategory = 35;
+const int effGetEffectName = 45;
+const int effGetVendorString = 47;
+const int effGetProductString = 48;
+const int effGetVendorVersion = 49;
+const int effCanDo = 51; // currently unused
+/* from http://asseca.com/vst-24-specs/efIdle.html */
+const int effIdle = 53;
+/* from http://asseca.com/vst-24-specs/efGetParameterProperties.html */
+const int effGetParameterProperties = 56;
+const int effGetVstVersion = 58; // currently unused
+/* http://asseca.com/vst-24-specs/efShellGetNextPlugin.html */
+const int effShellGetNextPlugin = 70;
+/* The next two were gleaned from http://www.kvraudio.com/forum/printview.php?t=143587&start=0 */
+const int effStartProcess = 71;
+const int effStopProcess = 72;
+
+const int effBeginSetProgram = 67;
+const int effEndSetProgram = 68;
+
+const int kEffectMagic = CCONST( 'V', 's', 't', 'P' );
+const int kVstLangEnglish = 1;
+const int kVstMidiType = 1;
+
+const int kVstTransportChanged = 1;
+const int kVstTransportPlaying = 1 << 1;
+const int kVstTransportCycleActive = 1 << 2;
+const int kVstTransportRecording = 1 << 3; // currently unused
+const int kVstPpqPosValid = 1 << 9;
+const int kVstTempoValid = 1 << 10;
+const int kVstBarsValid = 1 << 11;
+const int kVstCyclePosValid = 1 << 12;
+const int kVstTimeSigValid = 1 << 13;
+const int kVstSmpteValid = 1 << 14; // currently unused
+const int kVstClockValid = 1 << 15; // currently unused
+
+// currently unused
+const int kVstSmpte24fps = 0;
+const int kVstSmpte25fps = 1;
+const int kVstSmpte2997fps = 2;
+const int kVstSmpte30fps = 3;
+const int kVstSmpte2997dfps = 4;
+const int kVstSmpte30dfps = 5;
+const int kVstSmpteFilm16mm = 6; // very likely
+const int kVstSmpteFilm35mm = 7; // very likely
+const int kVstSmpte239fps = 10;
+const int kVstSmpte249fps = 11;
+const int kVstSmpte599fps = 12;
+const int kVstSmpte60fps = 13;
+
+/* constants from http://asseca.com/vst-24-specs/efGetPlugCategory.html */
+
+enum VstPlugCategory
+{
+        kPlugCategUnknown = 0,
+        kPlugCategEffect,
+        kPlugCategSynth,
+        kPlugCategAnalysis,
+        kPlugCategMastering,
+        kPlugCategSpacializer,
+        kPlugCategRoomFx,
+        kPlugSurroundFx,
+        kPlugCategRestoration,
+        kPlugCategOfflineProcess,
+        kPlugCategShell,
+        kPlugCategGenerator,
+        kPlugCategMaxCount
+};
+
+
+
+class VstMidiEvent
+{
+public:
+	// 00
+	int type;
+	// 04
+	int byteSize;
+	// 08
+	int deltaFrames;
+	// 0c?
+	int flags;
+	// 10?
+	int noteLength;
+	// 14?
+	int noteOffset;
+	// 18
+	char midiData[4];
+	// 1c?
+	char detune;
+	// 1d?
+	char noteOffVelocity;
+	// 1e?
+	char reserved1;
+	// 1f?
+	char reserved2;
+
+} ;
+
+
+
+
+class VstEvent
+{
+	char dump[sizeof( VstMidiEvent )];
+
+} ;
+
+
+
+
+class VstEvents
+{
+public:
+	// 00
+	int numEvents;
+	// 04
+	void *reserved;
+	// 08
+	VstEvent* events[1];
+
+} ;
+
+
+class AEffect
+{
+public:
+	// Never use virtual functions!!!
+	// 00-03
+	int magic;
+	// dispatcher 04-07
+	intptr_t (* dispatcher)( AEffect * , int , int , intptr_t, void * , float );
+	// process, quite sure 08-0b
+	void (* process)( AEffect * , float * * , float * * , int );
+	// setParameter 0c-0f
+	void (* setParameter)( AEffect * , int , float );
+	// getParameter 10-13
+	float (* getParameter)( AEffect * , int );
+	// programs 14-17
+	int numPrograms;
+	// Params 18-1b
+	int numParams;
+	// Input 1c-1f
+	int numInputs;
+	// Output 20-23
+	int numOutputs;
+	// flags 24-27
+	int flags;
+	// Fill somewhere 28-2b
+	void *ptr1;
+	void *ptr2;
+	// Zeroes 2c-2f 30-33 34-37 38-3b
+	char empty3[4 + 4 + 4];
+	// 1.0f 3c-3f
+	float unknown_float;
+	// An object? pointer 40-43
+	void *ptr3;
+	// Zeroes 44-47
+	void *user;
+	// Id 48-4b
+	int32_t uniqueID;
+	// Don't know 4c-4f
+	char unknown1[4];
+	// processReplacing 50-53
+	void (* processReplacing)( AEffect * , float * * , float * * , int );
+
+} ;
+
+
+
+
+class VstTimeInfo
+{
+public:
+	// 00
+	double samplePos;
+	// 08
+	double sampleRate;
+	// unconfirmed 10
+	double nanoSeconds;
+	// 18
+	double ppqPos;
+	// 20
+	double tempo;
+	// 28
+	double barStartPos;
+	// 30
+	double cycleStartPos;
+	// 38
+	double cycleEndPos;
+	// 40
+	int32_t timeSigNumerator;
+	// 44
+	int32_t timeSigDenominator;
+	// 48 unused
+	int32_t smpteOffset;
+	// 4c unused
+	int32_t smpteFrameRate;
+	// 50? unused, where does this come from?
+	int32_t samplesToNextClock;
+	// 54
+	int32_t flags;
+
+} ;
+
+
+
+typedef intptr_t (* audioMasterCallback)( AEffect * , int32_t, int32_t, intptr_t, void * , float );
+
+
+#endif
