d5fae85f482d8274d0deac51d0d65c1c44109613
Stefan Westerfeld <stefan@space.twc.de>
2016-09-10 16:47:23 +0200

    LIB: implement process function (which adds computed result to output buffer)

--- vst/smvstplugin.cc
+++ vst/smvstplugin.cc
@@ -337,18 +337,8 @@ static intptr_t dispatcher(AEffect *effect, int opcode, int index, intptr_t val,
   }
 }
 
-static void process(AEffect *effect, float **inputs, float **outputs, int numSampleFrames)
-{
-  VstPlugin *plugin = (VstPlugin *)effect->ptr3;
-  debug ("!missing process\n");
-#if 0 // FIXME
-  std::vector<amsynth_midi_cc_t> midi_out;
-  plugin->synthesizer->process(numSampleFrames, plugin->midiEvents, midi_out, outputs[0], outputs[1]);
-  plugin->midiEvents.clear();
-#endif
-}
-
-static void processReplacing(AEffect *effect, float **inputs, float **outputs, int numSampleFrames)
+static void
+processReplacing (AEffect *effect, float **inputs, float **outputs, int numSampleFrames)
 {
   VstPlugin *plugin = (VstPlugin *)effect->ptr3;
 
@@ -370,6 +360,21 @@ static void processReplacing(AEffect *effect, float **inputs, float **outputs, i
   const float volume_factor = bse_db_to_factor (plugin->parameters[VstPlugin::PARAM_VOLUME].value);
   for (int i = 0; i < numSampleFrames; i++)
     outputs[0][i] *= volume_factor;
+}
+
+static void
+process (AEffect *effect, float **inputs, float **outputs, int numSampleFrames)
+{
+  // this is not as efficient as it could be
+  // however, most likely the actual morphing is a lot more expensive
+
+  float  tmp_output[numSampleFrames];
+  float *tmp_outputs[1] = { tmp_output };
+
+  processReplacing (effect, inputs, tmp_outputs, numSampleFrames);
+
+  for (int i = 0; i < numSampleFrames; i++)
+    outputs[0][i] += tmp_output[i];
 }
 
 static void setParameter(AEffect *effect, int i, float f)
