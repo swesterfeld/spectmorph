189ca15acb321dc43aa82efeae2d1f3733a39d6c
Stefan Westerfeld <stefan@space.twc.de>
2016-12-18 18:19:55 +0100

    GUI: added GUI for ADSR envelope
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- gui/smmorphoutputview.cc
+++ gui/smmorphoutputview.cc
@@ -3,6 +3,7 @@
 #include "smmorphoutputview.hh"
 #include "smmorphplan.hh"
 #include "smutils.hh"
+#include <functional>
 
 #include <QLabel>
 
@@ -21,6 +22,48 @@ struct MyOperatorFilter : public OperatorFilter
   }
 } op_filter_instance;
 
+}
+
+void
+PropView::setVisible (bool visible)
+{
+  title->setVisible (visible);
+  slider->setVisible (visible);
+  label->setVisible (visible);
+}
+
+PropView
+xprop (QGridLayout *grid_layout, int row,
+       const std::string& title, float min, float max, int decimal_factor, const std::string& fmt,
+       MorphOutput *morph_op,
+       std::function<float(const MorphOutput&)> get,
+       std::function<void (MorphOutput&, float)> set)
+{
+  PropView pv;
+
+  pv.title = new QLabel (title.c_str());
+
+  pv.slider = new QSlider (Qt::Horizontal);
+  pv.slider->setRange (lrint (min * decimal_factor), lrint (max * decimal_factor));
+
+  const int value = lrint (get (*morph_op) * decimal_factor);
+  pv.slider->setValue (value);
+
+  pv.label = new QLabel();
+  pv.label->setText (string_locale_printf (fmt.c_str(), double (value) / decimal_factor).c_str());
+
+  QObject::connect (pv.slider, &QSlider::valueChanged,
+    [=](int value) {
+      float f = double (value) / decimal_factor;
+      pv.label->setText (string_locale_printf (fmt.c_str(), f).c_str());
+      set (*morph_op, f);
+    });
+
+  grid_layout->addWidget (pv.title, row, 0);
+  grid_layout->addWidget (pv.slider, row, 1);
+  grid_layout->addWidget (pv.label, row, 2);
+
+  return pv;
 }
 
 MorphOutputView::MorphOutputView (MorphOutput *morph_output, MorphPlanWindow *morph_plan_window) :
@@ -72,26 +115,25 @@ MorphOutputView::MorphOutputView (MorphOutput *morph_output, MorphPlanWindow *mo
   on_unison_voices_changed (unison_voices_value);
 
   // Unison Detune
-  unison_detune_slider = new QSlider (Qt::Horizontal);
-  unison_detune_slider->setRange (5, 500);
-  connect (unison_detune_slider, SIGNAL (valueChanged (int)), this, SLOT (on_unison_detune_changed (int)));
-  unison_detune_label = new QLabel();
-  unison_detune_title = new QLabel ("Detune");
+  pv_unison_detune = xprop (grid_layout, 8, "Detune", 0.5, 50, 10, "%.1f Cent", morph_output, &MorphOutput::unison_detune, &MorphOutput::set_unison_detune);
 
-  grid_layout->addWidget (unison_detune_title, 8, 0);
-  grid_layout->addWidget (unison_detune_slider, 8, 1);
-  grid_layout->addWidget (unison_detune_label, 8, 2);
+  // ADSR
+  QCheckBox *adsr_check_box = new QCheckBox ("Enable custom ADSR Envelope");
+  adsr_check_box->setChecked (morph_output->adsr());
+  grid_layout->addWidget (adsr_check_box, 9, 0, 1, 2);
 
-  const int unison_detune_value = lrint (morph_output->unison_detune() * 10);
-  unison_detune_slider->setValue (unison_detune_value);
-  on_unison_detune_changed (unison_detune_value);
+  pv_adsr_skip = xprop (grid_layout, 10,   "Skip",      0, 500, 10, "%.1f ms", morph_output, &MorphOutput::adsr_skip,     &MorphOutput::set_adsr_skip);
+  pv_adsr_attack = xprop (grid_layout, 11,   "Attack",    0, 100, 10, "%.1f %%", morph_output, &MorphOutput::adsr_attack,   &MorphOutput::set_adsr_attack);
+  pv_adsr_decay = xprop (grid_layout, 12,   "Decay",     0, 100, 10, "%.1f %%", morph_output, &MorphOutput::adsr_decay,    &MorphOutput::set_adsr_decay);
+  pv_adsr_sustain = xprop (grid_layout, 13,   "Sustain",   0, 100, 10, "%.1f %%", morph_output, &MorphOutput::adsr_sustain,  &MorphOutput::set_adsr_sustain);
+  pv_adsr_release = xprop (grid_layout, 14,   "Release",   0, 100, 10, "%.1f %%", morph_output, &MorphOutput::adsr_release,  &MorphOutput::set_adsr_release);
 
-  // Unison: initial widget visibility
-  on_chorus_changed (morph_output->chorus());
+  update_visibility();
 
   connect (sines_check_box, SIGNAL (toggled (bool)), this, SLOT (on_sines_changed (bool)));
   connect (noise_check_box, SIGNAL (toggled (bool)), this, SLOT (on_noise_changed (bool)));
   connect (chorus_check_box, SIGNAL (toggled (bool)), this, SLOT (on_chorus_changed (bool)));
+  connect (adsr_check_box, SIGNAL (toggled (bool)), this, SLOT (on_adsr_changed (bool)));
   setLayout (grid_layout);
 }
 
@@ -112,13 +154,7 @@ MorphOutputView::on_chorus_changed (bool new_value)
 {
   morph_output->set_chorus (new_value);
 
-  unison_voices_title->setVisible (new_value);
-  unison_voices_label->setVisible (new_value);
-  unison_voices_slider->setVisible (new_value);
-
-  unison_detune_title->setVisible (new_value);
-  unison_detune_label->setVisible (new_value);
-  unison_detune_slider->setVisible (new_value);
+  update_visibility();
 }
 
 void
@@ -129,11 +165,26 @@ MorphOutputView::on_unison_voices_changed (int voices)
 }
 
 void
-MorphOutputView::on_unison_detune_changed (int new_value)
+MorphOutputView::on_adsr_changed (bool new_value)
 {
-  const double detune = new_value / 10.0;
-  unison_detune_label->setText (string_locale_printf ("%.1f Cent", detune).c_str());
-  morph_output->set_unison_detune (detune);
+  morph_output->set_adsr (new_value);
+
+  update_visibility();
+}
+
+void
+MorphOutputView::update_visibility()
+{
+  unison_voices_title->setVisible (morph_output->chorus());
+  unison_voices_label->setVisible (morph_output->chorus());
+  unison_voices_slider->setVisible (morph_output->chorus());
+  pv_unison_detune.setVisible (morph_output->chorus());
+
+  pv_adsr_skip.setVisible (morph_output->adsr());
+  pv_adsr_attack.setVisible (morph_output->adsr());
+  pv_adsr_decay.setVisible (morph_output->adsr());
+  pv_adsr_sustain.setVisible (morph_output->adsr());
+  pv_adsr_release.setVisible (morph_output->adsr());
 }
 
 void
