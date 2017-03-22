a60d9e249fecf4083d253fef80b7a3eddebd681c
Stefan Westerfeld <stefan@space.twc.de>
2017-03-22 17:21:52 +0100

    TOOLS: add simple pbuilder shell script for i386 + amd64 .deb creation
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

---
+++ tools/pbuilder.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+if [ “$(id -u)” != “0” ]; then
+  echo “This script must be run as root” 2>&1
+  exit 1
+fi
+
+#----------------------------------------
+# CONFIG FILE CONTENTS: ~/.pbuilderrc
+#----------------------------------------
+# COMPONENTS="main restricted universe multiverse"
+# CCACHEDIR=/var/cache/pbuilder/ccache
+#----------------------------------------
+
+DSC=../../spectmorph-nobse_0.3.2.dsc
+
+unset CC
+unset CXX
+
+for DIST in xenial
+do
+  for ARCH in i386 amd64
+  do
+    BASETGZ=/var/cache/pbuilder/$DIST-$ARCH-base.tgz
+
+    if test -f $BASETGZ; then
+      echo using existing basetgz $BASETGZ
+    else
+      pbuilder --create                \
+               --distribution $DIST    \
+               --architecture $ARCH    \
+               --basetgz $BASETGZ
+    fi
+    pbuilder --build                \
+             --distribution $DIST   \
+             --architecture $ARCH   \
+             --basetgz $BASETGZ     \
+             $DSC
+  done
+done
