09b29ca30d57dfe8fa406b8ba3342adc5dec5628
Stefan Westerfeld <stefan@space.twc.de>
2017-03-26 21:28:52 +0200

    INSPECTOR: replace Bse::Error with SpectMorph::Error where appropriate
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- inspector/smnavigator.cc
+++ inspector/smnavigator.cc
@@ -215,10 +215,10 @@ Navigator::on_combo_changed()
   wset_filename = new_filename;
   wset_edit = false;
   wset_active_text = smset_combobox->currentText().toLatin1().data();
-  Bse::Error error = wset.load (wset_filename);
+  Error error = wset.load (wset_filename);
   if (error != 0)
     {
-      fprintf (stderr, "sminspector: can't open input file: %s: %s\n", wset_filename.c_str(), bse_error_blurb (error));
+      fprintf (stderr, "sminspector: can't open input file: %s: %s\n", wset_filename.c_str(), sm_error_blurb (error));
       exit (1);
     }
 
@@ -351,10 +351,10 @@ Navigator::on_save_clicked()
 {
   if (wset_filename != "")
     {
-      Bse::Error error = wset.save (wset_filename);
+      Error error = wset.save (wset_filename);
       if (error != 0)
         {
-          fprintf (stderr, "sminspector: can't write output file: %s: %s\n", wset_filename.c_str(), bse_error_blurb (error));
+          fprintf (stderr, "sminspector: can't write output file: %s: %s\n", wset_filename.c_str(), sm_error_blurb (error));
           exit (1);
         }
       wset_edit = false;
