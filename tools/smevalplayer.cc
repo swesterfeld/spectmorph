1bef776b23aa7d580cf3e9187aaa0a01981d3113
Stefan Westerfeld <stefan@space.twc.de>
2017-04-03 17:26:32 +0200

    TOOLS: smevalplayer: use WavData to load samples
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tools/smevalplayer.cc
+++ tools/smevalplayer.cc
@@ -2,7 +2,7 @@
 
 #include "smmain.hh"
 #include "smsimplejackplayer.hh"
-#include "smwavloader.hh"
+#include "smwavdata.hh"
 
 #include <stdio.h>
 #include <string.h>
@@ -39,22 +39,20 @@ command_loop()
         {
           string filename = arg;
 
-          WavLoader *wav_loader = WavLoader::load (filename);
-          if (wav_loader)
+          WavData wav_data;
+          if (wav_data.load_mono (filename))
             {
               Audio audio;
-              audio.mix_freq = wav_loader->mix_freq();
+              audio.mix_freq = wav_data.mix_freq();
               audio.fundamental_freq = 440; /* doesn't matter */
-              audio.original_samples = wav_loader->samples();
+              audio.original_samples = wav_data.samples();
 
               if (debug)
                 {
-                  bool need_resample = fabs (wav_loader->mix_freq() - jack_player.mix_freq()) > 1e-6;
+                  bool need_resample = fabs (wav_data.mix_freq() - jack_player.mix_freq()) > 1e-6;
                   printf ("  -> resample = %s\n", need_resample ? "true" : "false");
                 }
               jack_player.play (&audio, true);
-
-              delete wav_loader;
             }
         }
       if (strcmp (cmd, "stop") == 0 && !arg)
