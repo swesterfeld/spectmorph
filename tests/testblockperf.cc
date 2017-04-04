c15f301c022b7028870e8ec994aabbcfd750656b
Stefan Westerfeld <stefan@space.twc.de>
2017-04-04 22:05:48 +0200

    TESTS: provide Block::add | Block::mul perf test
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

---
+++ tests/testblockperf.cc
@@ -0,0 +1,81 @@
+// Licensed GNU LGPL v3 or later: http://www.gnu.org/licenses/lgpl.html
+
+#include "smmain.hh"
+#include "smrandom.hh"
+#include "smfft.hh"
+
+#include <bse/bsemathsignal.hh>
+#include <bse/bseblockutils.hh>
+
+#include <assert.h>
+#include <sys/time.h>
+
+using namespace SpectMorph;
+using Rapicorn::AlignedArray;
+using std::max;
+using std::min;
+using std::vector;
+
+static double
+gettime()
+{
+  timeval tv;
+  gettimeofday (&tv, 0);
+
+  return tv.tv_sec + tv.tv_usec / 1000000.0;
+}
+
+static void
+block_perf (bool add, bool aligned)
+{
+  const size_t block_size = 4096;
+
+  AlignedArray<float, 16> a (block_size + 1);
+  AlignedArray<float, 16> b (block_size + 1);
+
+  // FIXME: could test with and without
+  // sm_enable_sse (sse);
+
+  Random random;
+  random.set_seed (42);
+  for (size_t i = 0; i < block_size; i++)
+    {
+      a[i] = random.random_double_range (-1.0, 1.0);
+      if (add)
+        b[i] = random.random_double_range (-1.0, 1.0);
+      else
+        b[i] = random.random_double_range (1.0, 1.0001);
+    }
+  double min_time = 1e20;
+  const int RUNS = 20000, REPS = 13;
+  for (int reps = 0; reps < REPS; reps++)
+    {
+      double start = gettime();
+      for (int r = 0; r < RUNS; r++)
+        {
+          if (add)
+            Bse::Block::add (block_size, &a[aligned ? 0 : 1], &b[0]);
+          else
+            Bse::Block::mul (block_size, &a[aligned ? 0 : 1], &b[0]);
+        }
+      double end = gettime();
+      min_time = min (min_time, end - start);
+    }
+
+  const double ns_per_sec = 1e9;
+  const double time_norm = ns_per_sec / RUNS / block_size;
+
+  printf ("%s %s %f ns/sample\n", add ? "add" : "mul", aligned ? "  aligned" : "unaligned", min_time * time_norm);
+}
+
+int
+main (int argc, char **argv)
+{
+  sm_init (&argc, &argv);
+
+  block_perf (true,  false);
+  block_perf (false, false);
+  printf ("------------------------\n");
+  block_perf (true,  true);
+  block_perf (false, true);
+}
