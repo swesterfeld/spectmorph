aa011618cf9fe2f9d5e9b5fc68845dcce0b37855
Stefan Westerfeld <stefan@space.twc.de>
2017-04-05 13:54:15 +0200

    LIB: provide SpectMorph::AlignedArray (replaces Rapicorn::AlignedArray)
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

---
+++ lib/smalignedarray.cc
@@ -0,0 +1,37 @@
+// Licensed GNU LGPL v3 or later: http://www.gnu.org/licenses/lgpl.html
+
+#include "smalignedarray.hh"
+
+#include <glib.h>
+
+using namespace SpectMorph;
+
+/* --- memory utils --- */
+void*
+malloc_aligned (gsize     total_size,
+                gsize     alignment,
+                guint8  **free_pointer)
+{
+  const bool  alignment_power_of_2 = (alignment & (alignment - 1)) == 0;
+  const gsize cache_line_size = 64; // ensure that no false sharing will occur (at begin and end of data)
+  if (alignment_power_of_2)
+    {
+      // for power of 2 alignment, we guarantee also cache line alignment
+      alignment = std::max (alignment, cache_line_size);
+      uint8 *aligned_mem = (uint8 *) g_malloc (total_size + (alignment - 1) + (cache_line_size - 1));
+      *free_pointer = aligned_mem;
+      if ((ptrdiff_t) aligned_mem % alignment)
+        aligned_mem += alignment - (ptrdiff_t) aligned_mem % alignment;
+      return aligned_mem;
+    }
+  else
+    {
+      uint8 *aligned_mem = (uint8 *) g_malloc (total_size + (alignment - 1) + (cache_line_size - 1) * 2);
+      *free_pointer = aligned_mem;
+      if ((ptrdiff_t) aligned_mem % cache_line_size)
+        aligned_mem += cache_line_size - (ptrdiff_t) aligned_mem % cache_line_size;
+      if ((ptrdiff_t) aligned_mem % alignment)
+        aligned_mem += alignment - (ptrdiff_t) aligned_mem % alignment;
+      return aligned_mem;
+    }
+}
