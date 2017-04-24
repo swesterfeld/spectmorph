a538b6c2d2e19dccd0ffcce02dcac69ba865ea09
Stefan Westerfeld <stefan@space.twc.de>
2017-04-24 11:05:03 +0200

    BUILD: remove automatic ChangeLog generation
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- autogen.sh
+++ autogen.sh
@@ -24,8 +24,5 @@ fi
 # bail out as scheduled
 test "0$DIE" -gt 0 && exit 1
 
-echo "Ensure: ChangeLog"
-test -e ChangeLog || TZ=GMT0 touch ChangeLog -t 190112132145.52 # automake *requires* ChangeLog
-
 echo "Running: autoreconf -i && ./configure $@"
 autoreconf -i -Wno-portability && ./configure "$@"
