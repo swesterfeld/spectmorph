cc88ff6da0696f62dcb11278e5b05d08e78c4de6
Stefan Westerfeld <stefan@space.twc.de>
2017-05-08 12:46:09 +0200

    TOOLS: add portamento test script
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

---
+++ tools/testporta.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$#" -ne 4 ]; then
+  echo "usage: porta.sh <inst> <from> <to> <delta>"
+  exit 1
+fi
+
+INST=$1
+FROM=$2
+TO=$3
+DELTA=$4
+
+END_SLIDE=$(python -c 'print 1.0 + '$DELTA)
+END_TIME=$(python -c 'print 2.0 + '$DELTA)
+
+(
+echo "0.0 $FROM"
+echo "1.0 $FROM"
+echo "$END_SLIDE $TO"
+echo "$END_TIME $TO"
+) > /tmp/porta
+
+smlive $INST -f $FROM --loop $END_TIME --freq-in /tmp/porta -x /tmp/porta.wav
+gst123 -a jack /tmp/porta.wav
