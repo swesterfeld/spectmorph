6c38d8b6bebe35eec6261341e0b0cb95c813f89a
Stefan Westerfeld <stefan@space.twc.de>
2017-05-17 13:49:53 +0200

    TOOLS: polyphase fir design: refactor, cleanup
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tools/polyphasefir.py
+++ tools/polyphasefir.py
@@ -3,25 +3,50 @@
 # FIR design for polyphase filter
 
 from scipy import signal
+import numpy
+import sys
 
 OVERSAMPLE = 64
 SR = 48000.0
 LP_START = 16000.0
 LP_END   = 32000.0
 
+def find_beta (WIDTH):
+  best_beta = 0
+  best_max_h = 1
+  for beta in numpy.arange (0, 10, 0.1):
+    b = signal.firwin (WIDTH * 2 * OVERSAMPLE + 1, 1. / OVERSAMPLE, window=('kaiser', beta))
+    w, h = signal.freqz (b, worN=2**14)
+
+    max_h = 0
+    for p in range (len (h)):
+      freq = (24000.0 * p) / len (h)
+
+      if freq > 32000. / OVERSAMPLE:
+        max_h = max (max_h, abs (h[p]))
+    print >> sys.stderr, beta, max_h
+    if (max_h < best_max_h):
+      best_beta = beta
+      best_max_h = max_h
+  return best_beta
+
 def design (WIDTH):
-  #return signal.firwin (WIDTH * 2 * OVERSAMPLE + 1, 1. / OVERSAMPLE, window=('kaiser', 7.5)) <- 7
-  #return signal.firwin (WIDTH * 2 * OVERSAMPLE + 1, 1. / OVERSAMPLE, window=('kaiser', 5.0)) <- 5
-  return signal.firwin (WIDTH * 2 * OVERSAMPLE + 1, 1. / OVERSAMPLE, width = 0.5 / OVERSAMPLE)
+  beta = find_beta (WIDTH)
+  return signal.firwin (WIDTH * 2 * OVERSAMPLE + 1, 1. / OVERSAMPLE, window=('kaiser', beta))
   #return signal.remez (WIDTH * 2 * OVERSAMPLE + 1, [0, (LP_START / SR) / OVERSAMPLE, (LP_END / SR) / OVERSAMPLE, 0.5], [1, 0])
 
-w, h = signal.freqz (design (8), worN=2**14)
+def dump_coefficients():
+ for WIDTH in range (1, 10):
+   print "const double c_%d[%d] = {" % (WIDTH, WIDTH * 2 * OVERSAMPLE + 1)
+   for d in design (WIDTH):
+     print "  %.17g," % (d * OVERSAMPLE)
+   print "};"
 
-for p in range (len (h)):
-  print (24000.0 * p) / len (h), abs (h[p])
+def dump_plot (WIDTH):
+  w, h = signal.freqz (design (WIDTH), worN=2**14)
 
-# for WIDTH in range (1, 10):
-#   print "const double c_%d[%d] = {" % (WIDTH, WIDTH * 2 * OVERSAMPLE + 1)
-#   for d in design (WIDTH):
-#     print "  %.17g," % (d * OVERSAMPLE)
-#   print "};"
+  for p in range (len (h)):
+    print (24000.0 * p) / len (h), abs (h[p])
+
+dump_plot (int (sys.argv[1]))
+# dump_coefficients()
