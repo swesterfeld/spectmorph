73227f49511bdf40a78fb6df2805811af7220462
Stefan Westerfeld <stefan@space.twc.de>
2018-02-03 15:21:06 +0100

    GLUI: fix frame clipping problem
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- glui/smframe.hh
+++ glui/smframe.hh
@@ -15,30 +15,34 @@ struct Frame : public Widget
   void
   draw (cairo_t *cr) override
   {
-    static const double radius  = 5;
+    static const double radius  = 10;
     static const double degrees = 3.14159265 / 180.0;
+    static const double line_width = 1.5;
+    static const double lw_2 = line_width / 2;
 
     cairo_new_sub_path (cr);
     cairo_arc (cr,
-               width - radius,
-               radius,
+               width - lw_2 - radius,
+               lw_2 + radius,
                radius, -90 * degrees, 0 * degrees);
     cairo_arc (cr,
-                width - radius, height - radius,
+               width - lw_2 - radius,
+               height - lw_2 - radius,
                radius, 0 * degrees, 90 * degrees);
     cairo_arc (cr,
-               radius, height - radius,
+               lw_2 + radius,
+               height - lw_2 - radius,
                radius, 90 * degrees, 180 * degrees);
     cairo_arc (cr,
-                radius, radius,
+               lw_2 + radius,
+               lw_2 + radius,
                radius, 180 * degrees, 270 * degrees);
     cairo_close_path (cr);
 
     // Draw border
-    cairo_set_source_rgba(cr, 0.8, 0.8, 0.8, 1);
-    cairo_set_line_width(cr, 2.0);
-    cairo_stroke(cr);
-
+    cairo_set_source_rgba (cr, 0.8, 0.8, 0.8, 1);
+    cairo_set_line_width (cr, line_width);
+    cairo_stroke (cr);
 }
 };
 
