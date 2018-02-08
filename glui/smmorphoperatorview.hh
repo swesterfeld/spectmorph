6523b8c555e55aac0466723ac1ce37b336e94ef5
Stefan Westerfeld <stefan@space.twc.de>
2018-02-08 16:09:44 +0100

    GLUI: cosmetic changes in operator view
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- glui/smmorphoperatorview.hh
+++ glui/smmorphoperatorview.hh
@@ -21,14 +21,19 @@ public:
   {
     FixedGrid grid;
 
-    Label *label = new Label (this, op->type());
-    grid.add_widget (label, 1, 1, 20, 2);
+    // FIXME: need update (signal) on_operators_changed
+    std::string title = op->type_name() + ": " + op->name();
+
+    Label *label = new Label (this, title);
+    label->align = TextAlign::CENTER;
+    label->bold  = true;
+    grid.add_widget (label, 0, 0, 43, 4);
 
     Label *slider_label = new Label (this, "Attack");
     Slider *slider = new Slider (this, 0.5);
     Label *value_label = new Label (this, "50%");
 
-    int yoffset = 3;
+    int yoffset = 4;
     grid.add_widget (slider_label, 3, yoffset, 7, 2);
     grid.add_widget (slider,  10, yoffset, 27, 2);
     grid.add_widget (value_label, 38, yoffset, 5, 2);
