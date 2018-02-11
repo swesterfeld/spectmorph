b45f7749f9beacabb289c121443231dd322e7d50
Stefan Westerfeld <stefan@space.twc.de>
2018-02-11 14:39:04 +0100

    GLUI: make operator menu work
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

---
+++ glui/smmorphplanwindow.cc
@@ -0,0 +1,111 @@
+// Licensed GNU LGPL v3 or later: http://www.gnu.org/licenses/lgpl.html
+
+#include "smmorphplanwindow.hh"
+
+using namespace SpectMorph;
+
+MorphPlanWindow::MorphPlanWindow (int width, int height, PuglNativeWindow win_id, bool resize, MorphPlanPtr morph_plan) :
+  Window (width, height, win_id, resize),
+  m_morph_plan (morph_plan)
+{
+  FixedGrid grid;
+
+  MenuBar *menu_bar = new MenuBar (this);
+  Menu *file_menu = menu_bar->add_menu ("File");
+  Menu *preset_menu = menu_bar->add_menu ("Open Preset");
+  Menu *op_menu = menu_bar->add_menu ("Add Operator");
+  Menu *help_menu = menu_bar->add_menu ("Help");
+
+  auto set_items = [this](Menu *m, const std::vector<std::string>& items) {
+    for (auto i : items) {
+      MenuItem *item = m->add_item (i);
+      connect (item->signal_clicked, [=]()
+        {
+          printf ("menu item %s selected\n", i.c_str());
+        });
+    }
+  };
+  set_items (file_menu, {"Import...", "Export...", "Load Instrument Set..."});
+  fill_preset_menu (preset_menu);
+  add_op_menu_item (op_menu, "Source", "SpectMorph::MorphSource");
+  add_op_menu_item (op_menu, "Output", "SpectMorph::MorphOutput");
+  add_op_menu_item (op_menu, "Linear Morph", "SpectMorph::MorphLinear");
+  add_op_menu_item (op_menu, "Grid Morph", "SpectMorph::MorphGrid");
+  add_op_menu_item (op_menu, "LFO", "SpectMorph::MorphLFO");
+  set_items (help_menu, {"About..."});
+  grid.add_widget (menu_bar, 1, 1, 46, 3);
+
+  grid.add_widget (new MorphPlanView (this, morph_plan.c_ptr()), 1, 5, 44, 20);
+  grid.add_widget (new ScrollBar (this, 0.3), 45, 5, 2, 42);
+}
+
+void
+MorphPlanWindow::fill_preset_menu (Menu *menu)
+{
+  MicroConf cfg (sm_get_install_dir (INSTALL_DIR_TEMPLATES) + "/index.smpindex");
+
+  if (!cfg.open_ok())
+    {
+      return;
+    }
+
+  while (cfg.next())
+    {
+      std::string filename, title;
+
+      if (cfg.command ("plan", filename, title))
+        {
+          MenuItem *item = menu->add_item (title);
+          connect (item->signal_clicked, [=]()
+            {
+              on_load_preset (filename);
+            });
+        }
+    }
+}
+
+void
+MorphPlanWindow::add_op_menu_item (Menu *op_menu, const std::string& text, const std::string& type)
+{
+  MenuItem *item = op_menu->add_item (text);
+
+  connect (item->signal_clicked, [=]()
+    {
+      MorphOperator *op = MorphOperator::create (type, m_morph_plan.c_ptr());
+
+      g_return_if_fail (op != NULL);
+
+      m_morph_plan->add_operator (op, MorphPlan::ADD_POS_AUTO);
+    });
+}
+
+
+bool
+MorphPlanWindow::load (const std::string& filename)
+{
+  GenericIn *in = StdioIn::open (filename);
+  if (in)
+    {
+      m_morph_plan->load (in);
+      delete in; // close file
+
+      set_filename (filename);
+
+      return true;
+    }
+  return false;
+}
+
+void
+MorphPlanWindow::on_load_preset (const std::string& rel_filename)
+{
+  std::string filename = sm_get_install_dir (INSTALL_DIR_TEMPLATES) + "/" + rel_filename;
+
+  if (!load (filename))
+    {
+#if 0 // FIXME
+      QMessageBox::critical (this, "Error",
+                             string_locale_printf ("Loading template failed, unable to open file '%s'.", filename.c_str()).c_str());
+#endif
+    }
+}
--- glui/smmorphplanwindow.hh
+++ glui/smmorphplanwindow.hh
@@ -26,92 +26,13 @@ struct MorphPlanWindow : public Window
     m_filename = filename;
     // FIXME update_window_title();
   }
+  void add_op_menu_item (Menu *op_menu, const std::string& text, const std::string& op_name);
 public:
-  MorphPlanWindow (int width, int height, PuglNativeWindow win_id, bool resize, MorphPlanPtr morph_plan) :
-    Window (width, height, win_id, resize),
-    m_morph_plan (morph_plan)
-  {
-    FixedGrid grid;
+  MorphPlanWindow (int width, int height, PuglNativeWindow win_id, bool resize, MorphPlanPtr morph_plan);
 
-    MenuBar *menu_bar = new MenuBar (this);
-    Menu *file_menu = menu_bar->add_menu ("File");
-    Menu *preset_menu = menu_bar->add_menu ("Open Preset");
-    Menu *op_menu = menu_bar->add_menu ("Add Operator");
-    Menu *help_menu = menu_bar->add_menu ("Help");
-
-    auto set_items = [this](Menu *m, const std::vector<std::string>& items) {
-      for (auto i : items) {
-        MenuItem *item = m->add_item (i);
-        connect (item->signal_clicked, [=]()
-          {
-            printf ("menu item %s selected\n", i.c_str());
-          });
-      }
-    };
-    set_items (file_menu, {"Import...", "Export...", "Load Instrument Set..."});
-    fill_preset_menu (preset_menu);
-    set_items (op_menu, {"Source", "Output", "Linear Morph", "Grid Morph", "LFO" });
-    set_items (help_menu, {"About..."});
-    grid.add_widget (menu_bar, 1, 1, 46, 3);
-
-    grid.add_widget (new MorphPlanView (this, morph_plan.c_ptr()), 1, 5, 44, 20);
-    grid.add_widget (new ScrollBar (this, 0.3), 45, 5, 2, 42);
-  }
-  void
-  fill_preset_menu (Menu *menu)
-  {
-    MicroConf cfg (sm_get_install_dir (INSTALL_DIR_TEMPLATES) + "/index.smpindex");
-
-    if (!cfg.open_ok())
-      {
-        return;
-      }
-
-    while (cfg.next())
-      {
-        std::string filename, title;
-
-        if (cfg.command ("plan", filename, title))
-          {
-            MenuItem *item = menu->add_item (title);
-            connect (item->signal_clicked, [=]()
-              {
-                on_load_preset (filename);
-              });
-          }
-      }
-  }
-
-  bool
-  load (const std::string& filename)
-  {
-    GenericIn *in = StdioIn::open (filename);
-    if (in)
-      {
-        m_morph_plan->load (in);
-        delete in; // close file
-
-        set_filename (filename);
-
-        return true;
-      }
-    return false;
-  }
-
-  void
-  on_load_preset (const std::string& rel_filename)
-  {
-    std::string filename = sm_get_install_dir (INSTALL_DIR_TEMPLATES) + "/" + rel_filename;
-
-    if (!load (filename))
-      {
-#if 0 // FIXME
-        QMessageBox::critical (this, "Error",
-                               string_locale_printf ("Loading template failed, unable to open file '%s'.", filename.c_str()).c_str());
-#endif
-      }
-  }
-
+  void fill_preset_menu (Menu *menu);
+  bool load (const std::string& filename);
+  void on_load_preset (const std::string& rel_filename);
 };
 
 }
