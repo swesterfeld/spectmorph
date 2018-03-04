5d650814f134e5dd46ddf0c1f6d534dbdcffb6ca
Stefan Westerfeld <stefan@space.twc.de>
2018-03-04 14:54:45 +0100

    TESTS: small test for string formatting
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

---
+++ tests/teststrformat.cc
@@ -0,0 +1,17 @@
+// Licensed GNU LGPL v3 or later: http://www.gnu.org/licenses/lgpl.html
+
+#include "smutils.hh"
+#include "smmain.hh"
+
+using namespace SpectMorph;
+
+int
+main (int argc, char **argv)
+{
+  sm_init (&argc, &argv);
+
+  printf ("string_locale_printf:  %s", string_locale_printf ("%.2f\n", 3.14).c_str());
+  printf ("string_printf          %s", string_printf ("%.2f\n", 3.14).c_str());
+  printf ("sm_printf              ");
+  sm_printf ("%.2f\n", 3.14);
+}
