48688797af26782c1f3834781d0a4700db1ae862
Stefan Westerfeld <stefan@space.twc.de>
2018-03-15 10:48:31 +0100

    LIB: improve debug file output
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- lib/smdebug.cc
+++ lib/smdebug.cc
@@ -4,7 +4,10 @@
 #include "smutils.hh"
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 #include <set>
+#include <mutex>
+#include <atomic>
 
 using namespace SpectMorph;
 
@@ -13,29 +16,52 @@ using std::set;
 
 static set<string> active_areas;
 static FILE       *debug_file = NULL;
+static string      debug_filename = "smdebug.log";
+static std::mutex  debug_mutex;
+static std::atomic<int>  have_areas {0};
 
 void
 Debug::debug (const char *area, const char *fmt, ...)
 {
   // no debugging -> return as quickly as possible
-  if (active_areas.empty())
+  if (!have_areas.load())
     return;
 
+  std::lock_guard<std::mutex> locker (debug_mutex);
   if (active_areas.find (area) != active_areas.end())
     {
       if (!debug_file)
-        debug_file = fopen ("/tmp/smdebug.log", "w");
+        {
+          char *abs_filename = g_build_filename (g_get_tmp_dir(), debug_filename.c_str(), nullptr);
+          debug_file = fopen (abs_filename, "w");
+          g_free (abs_filename);
+        }
 
       va_list ap;
 
       va_start (ap, fmt);
       fprintf (debug_file, "%s", string_vprintf (fmt, ap).c_str());
       va_end (ap);
+
+      // hack: avoid fflush for encoder (since it writes lots of lines)
+      if (strcmp (area, "encoder") != 0)
+        fflush (debug_file);
     }
 }
 
 void
-Debug::debug_enable (const std::string& area)
+Debug::enable (const std::string& area)
 {
+  std::lock_guard<std::mutex> locker (debug_mutex);
+
   active_areas.insert (area);
+  have_areas.store (1);
+}
+
+void
+Debug::set_filename (const std::string& filename)
+{
+  std::lock_guard<std::mutex> locker (debug_mutex);
+
+  debug_filename = filename;
 }
--- lib/smdebug.hh
+++ lib/smdebug.hh
@@ -13,7 +13,8 @@ namespace Debug
 {
 
 void debug (const char *area, const char *fmt, ...) G_GNUC_PRINTF (2, 3);
-void debug_enable (const std::string& area);
+void enable (const std::string& area);
+void set_filename (const std::string& filename);
 
 }
 
