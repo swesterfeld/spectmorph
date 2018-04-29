a5379832a06dc239bdc6d17a6e451b72b4caa865
Stefan Westerfeld <stefan@space.twc.de>
2018-04-29 20:38:49 +0200

    LIB: added sm_atof(): locale independent string -> float conversion
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- lib/smmath.hh
+++ lib/smmath.hh
@@ -534,6 +534,13 @@ sm_bound (const T& min_value, const T& value, const T& max_value)
   return std::min (std::max (value, min_value), max_value);
 }
 
+inline double
+sm_atof (const char *nptr)
+{
+  /* do not use locale specific decimal separator */
+  return g_ascii_strtod (nptr, NULL);
+}
+
 } // namespace SpectMorph
 
 #endif
