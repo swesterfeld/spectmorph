463427e017ede21b67919bcaac649cae553fe03e
Stefan Westerfeld <stefan@space.twc.de>
2018-09-18 14:05:01 +0200

    GLUI: add simple Point class
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- glui/smwidget.hh
+++ glui/smwidget.hh
@@ -107,6 +107,32 @@ public:
   }
 };
 
+class Point
+{
+  double m_x;
+  double m_y;
+
+public:
+  Point() :
+    m_x (0), m_y (0)
+  {
+  }
+  Point (double x, double y) :
+    m_x (x), m_y (y)
+  {
+  }
+  double
+  x() const
+  {
+    return m_x;
+  }
+  double
+  y() const
+  {
+    return m_y;
+  }
+};
+
 enum class ThemeColor
 {
   FRAME,
