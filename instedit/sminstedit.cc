9a0a0547603351ca305d8f5600df903c346d18eb
Stefan Westerfeld <stefan@space.twc.de>
2018-09-26 14:12:13 +0200

    INSTEDIT: change marker colors
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- instedit/sminstedit.cc
+++ instedit/sminstedit.cc
@@ -222,22 +222,22 @@ public:
         if (m == MARKER_LOOP_START)
           {
             rect = Rect (marker_x, 0, 10, 10);
-            color = Color (1.0, 0.3, 0);
+            color = Color (0.7, 0.7, 1);
           }
         else if (m == MARKER_LOOP_END)
           {
             rect = Rect (marker_x - 10, 0, 10, 10);
-            color = Color (1.0, 0.3, 0);
+            color = Color (0.7, 0.7, 1);
           }
         else if (m == MARKER_CLIP_START)
           {
             rect = Rect (marker_x, height - 10, 10, 10);
-            color = Color (0.8, 0, 0);
+            color = Color (0.4, 0.4, 1);
           }
         else if (m == MARKER_CLIP_END)
           {
             rect = Rect (marker_x - 10, height - 10, 10, 10);
-            color = Color (0.8, 0, 0);
+            color = Color (0.4, 0.4, 1);
           }
         marker_rect[marker] = rect;
 
