29529072a99d97f3b1c5e87a3d7c11829f27bbcf
Stefan Westerfeld <stefan@space.twc.de>
2018-10-30 14:32:59 +0100

    GLUI: add missing SynthInterface include
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

---
+++ glui/smsynthinterface.hh
@@ -0,0 +1,36 @@
+// Licensed GNU LGPL v3 or later: http://www.gnu.org/licenses/lgpl.html
+
+#ifndef SPECTMORPH_SYNTH_INTERFACE_HH
+#define SPECTMORPH_SYNTH_INTERFACE_HH
+
+#include "smmidisynth.hh"
+namespace SpectMorph
+{
+
+class SynthInterface
+{
+public:
+  virtual
+  ~SynthInterface()
+  {
+  }
+  void
+  synth_inst_edit_update (bool active, const std::string& filename, bool original_samples)
+  {
+    synth_take_control_event (new InstEditUpdate (active, filename, original_samples));
+  }
+  void
+  synth_inst_edit_note (int note, bool on)
+  {
+    synth_take_control_event (new InstEditNote (note, on));
+  }
+  virtual void synth_take_control_event (SynthControlEvent *event) = 0;
+  virtual double synth_get_current_pos()
+  {
+    return -1;
+  }
+};
+
+}
+
+#endif
