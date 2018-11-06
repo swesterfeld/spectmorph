dc2ca79e7e28cb0efa16c39cfd52357b6d81bb35
Stefan Westerfeld <stefan@space.twc.de>
2018-11-06 17:07:25 +0100

    LIB: add AudioTool energy computation
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- lib/smaudiotool.cc
+++ lib/smaudiotool.cc
@@ -27,3 +27,31 @@ AudioTool::Block2Energy::energy (const AudioBlock& block)
 
   return e;
 }
+
+double
+AudioTool::compute_energy (const Audio& audio)
+{
+  double e = 0;
+  double e_norm = 0;
+
+  size_t start = 0;
+  size_t end   = audio.contents.size();
+
+  if (audio.loop_type == Audio::LoopType::LOOP_FRAME_FORWARD ||
+      audio.loop_type == Audio::LoopType::LOOP_FRAME_PING_PONG)
+    {
+      start = sm_bound<int> (start, audio.loop_start, end);
+
+      // last loop block is also used for normalization => (loop_end + 1)
+      end   = sm_bound<int> (start, audio.loop_end + 1, end);
+    }
+
+  AudioTool::Block2Energy block2energy (48000);
+
+  for (size_t b = start; b < end; b++)
+    {
+      e += block2energy.energy (audio.contents[b]);
+      e_norm += 1;
+    }
+  return e / e_norm;
+}
