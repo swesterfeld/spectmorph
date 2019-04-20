ddcba888af09196b14291877da66a7b04b20ed07
Stefan Westerfeld <stefan@space.twc.de>
2019-04-20 21:03:01 +0200

    GLUI: refactor control events
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- glui/smsynthinterface.hh
+++ glui/smsynthinterface.hh
@@ -44,16 +44,52 @@ public:
   ~SynthInterface()
   {
   }
+  template<class DATA>
+  void
+  send_control_event (const std::function<void(MidiSynth *)>& func, DATA *data = nullptr)
+  {
+    synth_take_control_event (new InstFunc (func, [data]() { delete data;}));
+  }
+  void
+  send_control_event (const std::function<void(MidiSynth *)>& func)
+  {
+    synth_take_control_event (new InstFunc (func, []() {}));
+  }
   void
   synth_inst_edit_update (bool active, WavSet *take_wav_set, bool original_samples)
   {
     /* ownership of take_wav_set is transferred to the event */
-    synth_take_control_event (new InstEditUpdate (active, take_wav_set, original_samples));
+    struct EventData
+    {
+      std::unique_ptr<WavSet> wav_set;
+    } *event_data = new EventData;
+
+    event_data->wav_set.reset (take_wav_set);
+
+    send_control_event (
+      [=] (MidiSynth *midi_synth)
+        {
+          midi_synth->set_inst_edit (active);
+
+          if (active)
+            midi_synth->inst_edit_synth()->take_wav_set (event_data->wav_set.release(), original_samples);
+        },
+      event_data);
   }
   void
   synth_inst_edit_note (int note, bool on)
   {
-    synth_take_control_event (new InstEditNote (note, on));
+    send_control_event (
+      [=] (MidiSynth *midi_synth)
+        {
+          unsigned char event[3];
+
+          event[0] = on ? 0x90 : 0x80;
+          event[1] = note;
+          event[2] = on ? 100 : 0;
+
+          midi_synth->add_midi_event (0, event);
+        });
   }
   virtual void synth_take_control_event (SynthControlEvent *event) = 0;
   virtual std::vector<std::string> notify_take_events() = 0;
