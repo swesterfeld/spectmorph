2ef10af299b43cead605cb482fc2a086f87b920c
Stefan Westerfeld <stefan@space.twc.de>
2019-05-05 22:49:05 +0200

    GUI: smfiledialog.sh: remove extra quotes
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- gui/smfiledialog.sh
+++ gui/smfiledialog.sh
@@ -4,7 +4,7 @@ SMFILEDIALOG=$(echo $0 | sed s/.sh//g)
 if [ "x$ARDOUR_BUNDLED" != "x" ]; then
   # at least kxStudio Ardour5.12.0 messes with LD_LIBRARY_PATH, which prevents the file dialog from opening
   # so we try to undo the modification before starting the file dialog
-  OLD_ENV="$(echo "$PREBUNDLE_ENV" | grep ^LD_LIBRARY_PATH=)"
+  OLD_ENV=$(echo "$PREBUNDLE_ENV" | grep ^LD_LIBRARY_PATH=)
   if [ "x$OLD_ENV" != "x" ]; then
     exec env "$OLD_ENV" "$SMFILEDIALOG" "$@"
   fi
