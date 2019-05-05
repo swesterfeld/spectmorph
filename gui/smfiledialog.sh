ba9533664de007e1c6a23ba3bbc4bd16a246e226
Stefan Westerfeld <stefan@space.twc.de>
2019-05-05 22:54:33 +0200

    GUI: another minor smfiledialog.sh fix
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- gui/smfiledialog.sh
+++ gui/smfiledialog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SMFILEDIALOG=$(echo $0 | sed s/.sh//g)
+SMFILEDIALOG=$(echo "$0" | sed 's/.sh$//g')
 if [ "x$ARDOUR_BUNDLED" != "x" ]; then
   # at least kxStudio Ardour5.12.0 messes with LD_LIBRARY_PATH, which prevents the file dialog from opening
   # so we try to undo the modification before starting the file dialog
