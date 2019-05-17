5a8ec38323a57d66d0a513280acd2f49dee1c987
Stefan Westerfeld <stefan@space.twc.de>
2019-05-17 11:29:18 +0200

    INSPECTOR: Error -> IError
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- inspector/smnavigator.cc
+++ inspector/smnavigator.cc
@@ -208,10 +208,10 @@ Navigator::on_combo_changed()
   wset_filename = new_filename;
   wset_edit = false;
   wset_active_text = smset_combobox->currentText().toLatin1().data();
-  Error error = wset.load (wset_filename);
-  if (error != 0)
+  IError error = wset.load (wset_filename);
+  if (error)
     {
-      fprintf (stderr, "sminspector: can't open input file: %s: %s\n", wset_filename.c_str(), sm_error_blurb (error));
+      fprintf (stderr, "sminspector: can't open input file: %s: %s\n", wset_filename.c_str(), error.message());
       exit (1);
     }
 
@@ -344,10 +344,10 @@ Navigator::on_save_clicked()
 {
   if (wset_filename != "")
     {
-      Error error = wset.save (wset_filename);
-      if (error != 0)
+      IError error = wset.save (wset_filename);
+      if (error)
         {
-          fprintf (stderr, "sminspector: can't write output file: %s: %s\n", wset_filename.c_str(), sm_error_blurb (error));
+          fprintf (stderr, "sminspector: can't write output file: %s: %s\n", wset_filename.c_str(), error.message());
           exit (1);
         }
       wset_edit = false;
