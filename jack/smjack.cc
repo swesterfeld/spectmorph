4a71c55e82e27f430619f0cda1c674aea9ed37ad
Stefan Westerfeld <stefan@space.twc.de>
2019-06-24 12:41:05 +0200

    JACK: replace sm_init with Main object
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- jack/smjack.cc
+++ jack/smjack.cc
@@ -92,7 +92,7 @@ JackSynth::JackSynth (jack_client_t *client, Project *project) :
 int
 main (int argc, char **argv)
 {
-  sm_init (&argc, &argv);
+  Main main (&argc, &argv);
 
   if (argc > 2)
     {
