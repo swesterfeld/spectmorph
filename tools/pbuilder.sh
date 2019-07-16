3fd73ea438211bd01cb1ec9fbbd52ae7207221ce
Stefan Westerfeld <stefan@space.twc.de>
2019-07-16 22:41:09 +0200

    TOOLS: use su automatically in pbuilder.sh
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tools/pbuilder.sh
+++ tools/pbuilder.sh
@@ -3,8 +3,8 @@
 set -e
 
 if [ “$(id -u)” != “0” ]; then
-  echo “This script must be run as root” 2>&1
-  exit 1
+  echo "This script must be run as root." 2>&1
+  exec su -c "$0"
 fi
 
 #----------------------------------------
