bd1f6289af1292f3bb9db8a1ce8a99f91b274c23
Stefan Westerfeld <stefan@space.twc.de>
2020-01-17 16:47:44 +0100

    TESTS: whitenoise.py -> python3
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tests/whitenoise.py
+++ tests/whitenoise.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import random
 
 # for deterministic tests
 random.seed (42)
 for i in range (20000):
-  print random.uniform (-0.5, 0.5)
+  print (random.uniform (-0.5, 0.5))
