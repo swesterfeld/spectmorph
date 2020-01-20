057807dc7fa29bafe885824bb773f27520dc4cd3
Stefan Westerfeld <stefan@space.twc.de>
2020-01-20 13:16:14 +0100

    STATIC: use static vst/lv2 build rules
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- static/build.sh
+++ static/build.sh
@@ -3,4 +3,5 @@ set -e
 source ./config.sh
 cd ..
 docker build -t spectmorph -f static/Dockerfile .
-docker run -v $PWD/static:/data -t spectmorph tar Cczfv /usr/local/spectmorph/lib/lv2 /data/spectmorph-$PACKAGE_VERSION-x86_64.tar.gz spectmorph.lv2
+mkdir -p static/bin
+docker run -v $PWD/static:/data -t spectmorph cp -av lv2/*.ttl vst/spectmorph_vst.so.static lv2/spectmorph_lv2.so.static /data/bin
