16c58e2a1da755acb512ff25d4293fd17e902789
Stefan Westerfeld <stefan@space.twc.de>
2020-09-22 14:38:51 +0200

    DOCS: fix pdf output for manual on focal
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- docs/manual.sh
+++ docs/manual.sh
@@ -1,6 +1,6 @@
 pandoc manual.md -o manual.html --css manual.css --number-sections --toc --template manual-template --self-contained
 pandoc manual.md -o manual.pdf --number-sections --toc \
 		--variable=subparagraph \
-		--latex-engine=xelatex -V mainfont='Charis SIL' -V mathfont=Asana-Math -V monofont=inconsolata \
+		--pdf-engine=xelatex -V mainfont='Charis SIL' -V mathfont=Asana-Math -V monofont=inconsolata \
 		-V fontsize=12pt -V papersize:a4 -V geometry:margin=2.5cm \
                 -H manual-pdf.tex
