6eb813116f6770538ec77b0e2915e8c1aab2a195
Stefan Westerfeld <stefan@space.twc.de>
2021-12-09 16:40:48 +0100

    LIB: rename local variables in SSE ARM _mm_shuffle_ps to avoid collisions
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- lib/smmatharm.hh
+++ lib/smmatharm.hh
@@ -39,15 +39,15 @@ inline __attribute__((always_inline)) __m128 _mm_set_ss(float a)
   return vsetq_lane_f32(a, vdupq_n_f32(0.f), 0);
 }
 
-#define _mm_shuffle_ps(ma, mb, imm8)                                                                           \
+#define _mm_shuffle_ps(a, b, imm8)                                                                             \
   __extension__({                                                                                              \
-    float32x4_t a = (ma);                                                                                      \
-    float32x4_t b = (mb);                                                                                      \
-    float32x4_t ret;                                                                                           \
-    ret = vmovq_n_f32(vgetq_lane_f32(a, (imm8) & (0x3)));                                                      \
-    ret = vsetq_lane_f32(vgetq_lane_f32(a, ((imm8) >> 2) & 0x3), ret, 1);                                      \
-    ret = vsetq_lane_f32(vgetq_lane_f32(b, ((imm8) >> 4) & 0x3), ret, 2);                                      \
-    ret = vsetq_lane_f32(vgetq_lane_f32(b, ((imm8) >> 6) & 0x3), ret, 3);                                      \
+    float32x4_t shuffle_ps_a_ = (a);                                                                           \
+    float32x4_t shuffle_ps_b_ = (b);                                                                           \
+    float32x4_t shuffle_ps_ret_;                                                                               \
+    shuffle_ps_ret_ = vmovq_n_f32(vgetq_lane_f32(shuffle_ps_a_, (imm8) & (0x3)));                              \
+    shuffle_ps_ret_ = vsetq_lane_f32(vgetq_lane_f32(shuffle_ps_a_, ((imm8) >> 2) & 0x3), shuffle_ps_ret_, 1);  \
+    shuffle_ps_ret_ = vsetq_lane_f32(vgetq_lane_f32(shuffle_ps_b_, ((imm8) >> 4) & 0x3), shuffle_ps_ret_, 2);  \
+    shuffle_ps_ret_ = vsetq_lane_f32(vgetq_lane_f32(shuffle_ps_b_, ((imm8) >> 6) & 0x3), shuffle_ps_ret_, 3);  \
   })
 #endif
 
