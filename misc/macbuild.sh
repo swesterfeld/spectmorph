e18d8c6e03d3e40a9fd3187dbf0cd698464b1fde
Stefan Westerfeld <stefan@space.twc.de>
2023-02-21 18:58:48 +0100

    MISC: add pkg-config for macOS builds
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- misc/macbuild.sh
+++ misc/macbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -Eeuo pipefail -x
 
-brew install autoconf-archive automake libsndfile jack lv2 fftw libao qt5
+brew install autoconf-archive automake pkg-config libsndfile jack lv2 fftw libao qt5
 export PKG_CONFIG_PATH="$(brew --prefix qt@5)/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
 ./autogen.sh
 make
