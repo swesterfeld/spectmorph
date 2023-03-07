305883805d73c6d13e7c8ff6ac351424bf009613
Stefan Westerfeld <stefan@space.twc.de>
2023-03-07 21:44:12 +0100

    TESTS: testnotifyperf: use new NotifyBuffer class from lib dir
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tests/testnotifyperf.cc
+++ tests/testnotifyperf.cc
@@ -5,71 +5,13 @@
 
 #include <vector>
 
-#include "smbinbuffer.hh"
+#include "smnotifybuffer.hh"
 #include "smmain.hh"
 #include "smmidisynth.hh"
 
 using namespace SpectMorph;
 using std::vector;
 using std::string;
-
-class NotifyBuffer
-{
-  std::vector<unsigned char> data;
-  size_t rpos = 0;
-
-  void
-  write_simple (const void *ptr, size_t size)
-  {
-    const unsigned char *raw_ptr = reinterpret_cast<const unsigned char *> (ptr);
-    data.insert (data.end(), raw_ptr, raw_ptr + size);
-  }
-  void
-  read_simple (void *ptr, size_t size)
-  {
-    unsigned char *raw_ptr = reinterpret_cast<unsigned char *> (ptr);
-    std::copy (data.begin() + rpos, data.begin() + rpos + size, raw_ptr);
-    rpos += size;
-  }
-public:
-  void
-  clear()
-  {
-    data.clear();
-    rpos = 0;
-  }
-  void
-  write_int (int i)
-  {
-    write_simple (&i, sizeof (i));
-  }
-  template<class T> void
-  write_seq (const T* items, size_t length)
-  {
-    write_int (length);
-    write_simple (items, length * sizeof (T));
-  }
-  size_t
-  remaining()
-  {
-    return data.size() - rpos;
-  }
-  int
-  read_int()
-  {
-    int i;
-    read_simple (&i, sizeof (i));
-    return i;
-  }
-  template<class T> std::vector<T>
-  read_seq()
-  {
-    int seq_len = read_int();
-    std::vector<T> result (seq_len);
-    read_simple (result.data(), seq_len * sizeof (T));
-    return result;
-  }
-};
 
 static int VOICE_OP_VALUES_EVENT = 642137; // just some random number
 
