bef51371726347af256cd591fec92612fad3c396
Stefan Westerfeld <stefan@space.twc.de>
2023-03-20 14:50:41 +0100

    STATIC: build and install CLAP plugin, too
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- static/build.sh
+++ static/build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 source ./config.sh
+
+OUT_DIR=spectmorph-$PACKAGE_VERSION-x86_64
+rm -rf $OUT_DIR
+mkdir -p $OUT_DIR
+
 cd ..
 docker build -t spectmorph -f static/Dockerfile .
 cd static
-OUT_DIR=spectmorph-$PACKAGE_VERSION-x86_64
-rm -rf $OUT_DIR
 
 # TEMPLATES / INSTRUMENTS
-mkdir -p $OUT_DIR
 docker run -v $PWD/$OUT_DIR:/data -t spectmorph cp -av \
   /usr/local/spectmorph/share/spectmorph/templates \
   /usr/local/spectmorph/share/spectmorph/instruments \
@@ -20,6 +22,11 @@ mkdir -p $OUT_DIR/vst
 docker run -v $PWD/$OUT_DIR:/data -t spectmorph cp -av \
   vst/spectmorph_vst.so.static /data/vst/spectmorph_vst.so
 
+# CLAP
+mkdir -p $OUT_DIR/clap
+docker run -v $PWD/$OUT_DIR:/data -t spectmorph cp -av \
+  clap/SpectMorph.clap.static /data/clap/SpectMorph.clap
+
 # LV2
 mkdir -p $OUT_DIR/lv2/spectmorph.lv2
 docker run -v $PWD/$OUT_DIR:/data -t spectmorph cp -av \
@@ -28,14 +35,14 @@ docker run -v $PWD/$OUT_DIR:/data -t spectmorph cp -av \
   /usr/local/spectmorph/lib/lv2/spectmorph.lv2/spectmorph.ttl \
   /usr/local/spectmorph/lib/lv2/spectmorph.lv2/manifest.ttl /data/lv2/spectmorph.lv2
 
-strip $OUT_DIR/vst/spectmorph_vst.so $OUT_DIR/lv2/spectmorph.lv2/spectmorph_lv2.so
+# ID
+docker run -v $PWD/$OUT_DIR:/data -t spectmorph chown -R $(id -u).$(id -g) /data
+
+strip $OUT_DIR/vst/spectmorph_vst.so $OUT_DIR/lv2/spectmorph.lv2/spectmorph_lv2.so $OUT_DIR/clap/SpectMorph.clap
 
 # INSTALLER
 cp -aiv install.sh $OUT_DIR
 chmod +x $OUT_DIR/install.sh
-
-# ID
-docker run -v $PWD/$OUT_DIR:/data -t spectmorph chown -R $(id -u).$(id -g) /data
 
 # TARBALL
 tar cfvJ $OUT_DIR.tar.xz $OUT_DIR
