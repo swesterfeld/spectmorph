51b90cb0506be436db3ed68ba0bcd9b54514bb95
Stefan Westerfeld <stefan@space.twc.de>
2023-03-20 15:26:24 +0100

    MISC: support building the installer directly from winbuild.sh
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- misc/winbuild.sh
+++ misc/winbuild.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-set -Eeuo pipefail
+set -Eeo pipefail
 
 docker build -f "misc/Dockerfile-win" -t spectmorph-dbuild-win .
-docker run -v $PWD:/data -t spectmorph-dbuild-win cp -av /usr/local/spectmorph-win/lib/vst/SpectMorph.dll /data/SpectMorph.dll
+if [ "x$1" == "xsetup" ]; then
+  docker run -v $PWD:/data -t spectmorph-dbuild-win bash -c '
+    cd windows
+    ./build.sh
+    ./mk-setup.sh
+    source ./config.sh
+    cp -av setup-spectmorph-${PACKAGE_VERSION}.exe /data'
+else
+  docker run -v $PWD:/data -t spectmorph-dbuild-win cp -av /usr/local/spectmorph-win/lib/vst/SpectMorph.dll /data/SpectMorph.dll
+  docker run -v $PWD:/data -t spectmorph-dbuild-win cp -av /usr/local/spectmorph-win/lib/clap/SpectMorph.clap /data/SpectMorph.clap
+fi
