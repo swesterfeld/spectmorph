2db7db38cf2311b2ada60eed836e91eaad28e9f8
Stefan Westerfeld <stefan@space.twc.de>
2023-04-06 14:28:22 +0200

    WINDOWS: use gcc12
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- windows/mxe-settings.mk
+++ windows/mxe-settings.mk
@@ -1,6 +1,6 @@
 ## https://github.com/mxe/mxe.git  ec0b69a6527e3babf30a13d71a150449418c273b
 MXE_TARGETS=x86_64-w64-mingw32.static
-MXE_PLUGIN_DIRS=plugins/gcc9
+MXE_PLUGIN_DIRS=plugins/gcc12
 JOBS=16
 
 LOCAL_PKG_LIST := libsndfile glib libao cairo fftw
