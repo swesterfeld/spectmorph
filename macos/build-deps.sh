4a656c7ecc71aca8f1dc28bbda33be4da4f7e791
Stefan Westerfeld <stefan@space.twc.de>
2023-04-13 17:03:12 +0200

    MACOS: make static build work on intel mac again

--- macos/build-deps.sh
+++ macos/build-deps.sh
@@ -40,7 +40,8 @@ mkdir -p $SRCDIR $BUILDD
 
 . sdk-options.sh
 PREFIX=$PWD/prefix
-GLOBAL_LDFLAGS="$SDK_OPTIONS -L$PREFIX/lib"
+GLOBAL_CFLAGS="$SDK_OPTIONS"
+GLOBAL_LDFLAGS="-L$PREFIX/lib"
 MAKEFLAGS="-j9"
 PATH=$PWD/prefix/bin:$PATH
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
@@ -88,7 +89,7 @@ autoconfbuild --disable-shared
 src cairo-1.15.10 tar.xz https://cairographics.org/snapshots/cairo-1.15.10.tar.xz
 patch -p1 < ../../cairo-1.15.10-notest.diff
 autoreconf -i
-autoconfbuild --disable-shared
+autoconfbuild --disable-shared --disable-xlib --disable-xcb
 
 src libogg-1.3.3 tar.xz http://downloads.xiph.org/releases/ogg/libogg-1.3.3.tar.xz
 autoconfbuild --disable-shared
