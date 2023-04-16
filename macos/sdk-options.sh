984794e32b588eb47c7d83d62f5b2a56d05d42ae
Stefan Westerfeld <stefan@space.twc.de>
2023-04-16 15:25:45 +0200

    MACOS: sdk-options.sh: support aarch64 builds

--- macos/sdk-options.sh
+++ macos/sdk-options.sh
@@ -3,6 +3,9 @@ if [ x$SDK_TARGET = "xx86_64" ]; then
   SDK_MINVERSION="10.9"
   SDK_OPTIONS="-isysroot $SDK_DIRECTORY -mmacosx-version-min=$SDK_MINVERSION -target x86_64-apple-macos10.9"
   SDK_AUTOCONF_BUILD="--build=x86_64-apple-darwin"
+elif [ x$SDK_TARGET = "xaarch64" ]; then
+  SDK_OPTIONS="-mmacosx-version-min=11 -target aarch64-apple-macos11"
+  SDK_AUTOCONF_BUILD="--build=aarch64-apple-darwin"
 else
   :
 fi
