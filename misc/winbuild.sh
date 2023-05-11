14e89ecdc8ece0fa6bd4fdc4fe6dfb8eca552df4
Stefan Westerfeld <stefan@space.twc.de>
2023-05-11 22:19:53 +0200

    MISC: don't create windows installer if build script fails
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- misc/winbuild.sh
+++ misc/winbuild.sh
@@ -5,10 +5,7 @@ docker build -f "misc/Dockerfile-win" -t spectmorph-dbuild-win .
 if [ "x$1" == "xsetup" ]; then
   docker run -v $PWD:/data -t spectmorph-dbuild-win bash -c '
     cd windows
-    ./build.sh
-    ./mk-setup.sh
-    source ./config.sh
-    cp -av setup-spectmorph-${PACKAGE_VERSION}.exe /data'
+    ./build.sh && ./mk-setup.sh && source ./config.sh && cp -av setup-spectmorph-${PACKAGE_VERSION}.exe /data'
 else
   docker run -v $PWD:/data -t spectmorph-dbuild-win cp -av /usr/local/spectmorph-win/lib/vst/SpectMorph.dll /data/
   docker run -v $PWD:/data -t spectmorph-dbuild-win cp -av /usr/local/spectmorph-win/lib/clap/SpectMorph.clap /data/
