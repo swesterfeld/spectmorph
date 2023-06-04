0a89a02f0f20f02004c5551ac77ef489373a2469
Stefan Westerfeld <stefan@space.twc.de>
2023-06-04 15:14:36 +0200

    MACOS: build aarch64 release against older SDK for better compatibility

--- macos/sdk-options.sh
+++ macos/sdk-options.sh
@@ -4,8 +4,12 @@ if [ x$SDK_TARGET = "xx86_64" ]; then
   SDK_OPTIONS="-isysroot $SDK_DIRECTORY -mmacosx-version-min=$SDK_MINVERSION -target x86_64-apple-macos10.9"
   SDK_AUTOCONF_BUILD="--build=x86_64-apple-darwin"
 elif [ x$SDK_TARGET = "xaarch64" ]; then
-  SDK_OPTIONS="-mmacosx-version-min=11 -target aarch64-apple-macos11"
+  SDK_DIRECTORY="$HOME/MacOSX11.3.sdk"
+  SDK_OPTIONS="-isysroot $SDK_DIRECTORY -mmacosx-version-min=11 -target aarch64-apple-macos11"
   SDK_AUTOCONF_BUILD="--build=aarch64-apple-darwin"
 else
   :
 fi
+
+# Compatibility: https://en.wikipedia.org/wiki/Xcode#Version_comparison_table
+# Download SDKs: https://github.com/phracker/MacOSX-SDKs
