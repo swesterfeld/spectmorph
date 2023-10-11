4877031d80fe1f35d77ff960ebb69a2b32fad76c
Stefan Westerfeld <stefan@space.twc.de>
2023-10-11 10:52:48 +0200

    MACOS: bump minimum system requirement to 10.15 (for std::filesystem)
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- macos/sdk-options.sh
+++ macos/sdk-options.sh
@@ -1,7 +1,7 @@
 if [ x$SDK_TARGET = "xx86_64" ]; then
-  SDK_DIRECTORY="$HOME/MacOSX10.9.sdk"
-  SDK_MINVERSION="10.9"
-  SDK_OPTIONS="-isysroot $SDK_DIRECTORY -mmacosx-version-min=$SDK_MINVERSION -target x86_64-apple-macos10.9"
+  SDK_DIRECTORY="$HOME/MacOSX10.15.sdk"
+  SDK_MINVERSION="10.15"
+  SDK_OPTIONS="-isysroot $SDK_DIRECTORY -mmacosx-version-min=$SDK_MINVERSION -target x86_64-apple-macos10.15"
   SDK_AUTOCONF_BUILD="--build=x86_64-apple-darwin"
 elif [ x$SDK_TARGET = "xaarch64" ]; then
   SDK_DIRECTORY="$HOME/MacOSX11.3.sdk"
