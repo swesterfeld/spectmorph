a81d6c25bc7476af18ac81121e6438f1eb0c0a37
Stefan Westerfeld <stefan@space.twc.de>
2023-10-31 13:41:43 +0100

    WINDOWS: build.sh: use instruments from instrument tarball
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- windows/build.sh
+++ windows/build.sh
@@ -14,6 +14,7 @@ make -C.. install || die "make install failed"
 cp -v $PREFIX/lib/vst/SpectMorph.dll . || die "error: cp SpectMorph.dll"
 cp -v $PREFIX/lib/clap/SpectMorph.clap . || die "error: cp SpectMorph.clap"
 cp -rv $PREFIX/lib/lv2/spectmorph.lv2 . || die "error: cp spectmorph.lv2"
+cp -rv $PREFIX/share/spectmorph/instruments . || die "error: cp instruments"
 
 for DIR in templates fonts
 do
