059a38fdf0f49c1c9a87814b88ce821d5e379ab1
Stefan Westerfeld <stefan@space.twc.de>
2023-11-25 18:45:28 +0100

    TESTS: testfftperf: there is no gsl fft anymore
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tests/testfftperf.cc
+++ tests/testfftperf.cc
@@ -40,8 +40,8 @@ time_fftsc()
 static double
 measure (const string& name, void (*func)(), bool is_complex)
 {
-  double clocks_per_sec = 2500.0 * 1000 * 1000;
-  unsigned int runs = 10000;
+  double ns_per_sec = 1e9;
+  unsigned int runs = 100000;
 
   // warmup run:
   func();
@@ -51,22 +51,9 @@ measure (const string& name, void (*func)(), bool is_complex)
   for (unsigned int i = 0; i < runs; i++)
     func();
   double end = get_time();
-  printf ("%s: %f clocks/sample\n", name.c_str(), clocks_per_sec * (end - start) / (is_complex ? (block_size / 2) : block_size) / runs);
+  printf ("%s: %f clocks/sample\n", name.c_str(), ns_per_sec * (end - start) / (is_complex ? (block_size / 2) : block_size) / runs);
 
   return (end - start);
-}
-
-static void
-compare (const string& name, void (*func)(), bool is_complex)
-{
-  FFT::use_gsl_fft (false);
-  double fftw = measure (name + "(fftw)", func, is_complex);
-
-  FFT::use_gsl_fft (true);
-  double gsl = measure (name + "(gsl)", func, is_complex);
-
-  printf (" => speedup: %.3f\n", gsl / fftw);
-  printf ("\n");
 }
 
 int
@@ -85,11 +72,11 @@ main (int argc, char **argv)
 
       printf ("========================= BLOCK_SIZE = %d =========================\n", block_size);
 
-      compare ("fftar", time_fftar, false);
-      compare ("fftsr", time_fftsr, false);
+      measure ("fftar", time_fftar, false);
+      measure ("fftsr", time_fftsr, false);
 
-      compare ("fftac", time_fftac, true);
-      compare ("fftsc", time_fftsc, true);
+      measure ("fftac", time_fftac, true);
+      measure ("fftsc", time_fftsc, true);
 
       FFT::free_array_float (in);
       FFT::free_array_float (out);
