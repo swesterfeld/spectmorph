83a01ebb1399336c02684e4a0d7ca5b4311b3a95
Stefan Westerfeld <stefan@space.twc.de>
2024-02-12 17:10:21 +0100

    MISC: macbuild.sh: use homebrew python via PATH
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- misc/macbuild.sh
+++ misc/macbuild.sh
@@ -3,6 +3,7 @@ set -Eeuo pipefail -x
 
 brew install autoconf-archive automake pkg-config libsndfile jack lv2 fftw libao qt5 scipy
 export PKG_CONFIG_PATH="$(brew --prefix qt@5)/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
+export PATH="$(brew --prefix python):$PATH"
 ./autogen.sh
 make -j `sysctl -n hw.ncpu`
 make check
