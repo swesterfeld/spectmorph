3c3367a57ded9a8e4789627a8f2e54b41327c31e
Stefan Westerfeld <stefan@space.twc.de>
2024-04-30 17:37:56 +0200

    MISC: macbuild.sh: install libtool as well
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- misc/macbuild.sh
+++ misc/macbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -Eeuo pipefail -x
 
-brew install autoconf-archive automake pkg-config libsndfile jack lv2 fftw libao qt5 scipy
+brew install autoconf-archive automake pkg-config libsndfile jack lv2 fftw libao qt5 scipy libtool
 export PKG_CONFIG_PATH="$(brew --prefix qt@5)/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
 export PATH="$(brew --prefix python)/bin:$PATH"
 ./autogen.sh
