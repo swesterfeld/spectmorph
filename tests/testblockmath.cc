75a2afd018504de7e72417abf20a1b986af4d3fd
Stefan Westerfeld <stefan@space.twc.de>
2024-06-05 14:43:13 +0200

    TESTS: testblockmath: split perf and accuracy test
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tests/testblockmath.cc
+++ tests/testblockmath.cc
@@ -15,11 +15,11 @@ using std::vector;
 float global_f = 0;
 
 double
-perf (bool fl2, int N)
+perf (bool fl2, unsigned int N)
 {
   vector<float> block (N);
   float f = 0.01;
-  const unsigned int runs = 200'000'000 / std::max (N, 16);
+  const unsigned int runs = 200'000'000 / std::max<uint> (N, 16);
   double start = get_time();
   for (unsigned int i = 0; i < runs; i++)
     {
@@ -46,28 +46,34 @@ main (int argc, char **argv)
 {
   Main main (&argc, &argv);
 
-  uint64_t K = 33452759; // prime
-  vector<float> x (K);
-  vector<float> expect (K);
-  for (size_t k = 0; k < K; k++)
+  if (argc == 1)
     {
-      /* range 2^-20 .. 2^20 */
-      double d = (double (k) / K) * 40 - 20;
-      expect[k] = d;
-      x[k] = exp2 (d);
+      uint64_t K = 33452759; // prime
+      vector<float> x (K);
+      vector<float> expect (K);
+      for (size_t k = 0; k < K; k++)
+        {
+          /* range 2^-20 .. 2^20 */
+          double d = (double (k) / K) * 40 - 20;
+          expect[k] = d;
+          x[k] = exp2 (d);
+        }
+      fast_log2 (x.data(), x.size());
+      double max_err = 0;
+      for (size_t k = 0; k < K; k++)
+        {
+          max_err = std::max (max_err, std::abs (double (x[k]) - double (expect[k])));
+        }
+      sm_printf ("max_err = %g\n", max_err);
+      assert (max_err < 3.82e-6);
     }
-  fast_log2 (x.data(), x.size());
-  double max_err = 0;
-  for (size_t k = 0; k < K; k++)
+  if (argc == 2 && !strcmp (argv[1], "perf"))
     {
-      max_err = std::max (max_err, std::abs (double (x[k]) - double (expect[k])));
-    }
-  sm_printf ("max_err = %f\n", max_err);
-
-  for (int N = 1; N <= 1024; N *= 2)
-    {
-      sm_printf ("N=%d\n", N);
-      sm_printf ("%9.4f fast_log2\n", 1e9 * perf (true, N));
-      sm_printf ("%9.4f log2f\n", 1e9 * perf (false, N));
+      for (int N = 1; N <= 1024; N *= 2)
+        {
+          sm_printf ("N=%d\n", N);
+          sm_printf ("%9.4f fast_log2\n", 1e9 * perf (true, N));
+          sm_printf ("%9.4f log2f\n", 1e9 * perf (false, N));
+        }
     }
 }
