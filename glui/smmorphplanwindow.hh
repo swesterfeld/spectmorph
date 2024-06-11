0f7772623d3330c6f0f7aa108f7d1cdb36593809
Stefan Westerfeld <stefan@space.twc.de>
2024-06-11 10:32:58 +0200

    GLUI: do not load contained wav sources for wav source preset
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- glui/smmorphplanwindow.hh
+++ glui/smmorphplanwindow.hh
@@ -29,7 +29,7 @@ class MorphPlanWindow : public Window
 
   void add_op_menu_item (Menu *op_menu, const std::string& text, const std::string& op_name);
 
-  Error load (const std::string& filename);
+  Error load (const std::string& filename, bool load_wav_sources);
   Error save (const std::string& filename);
 public:
   void
