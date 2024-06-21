ab4edaaa41c40b6e497bfe6e7b9e4543c1cc6e09
Stefan Westerfeld <stefan@space.twc.de>
2024-06-21 22:46:21 +0200

    MISC: adapt static test to ccached build
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- misc/staticbuild.sh
+++ misc/staticbuild.sh
@@ -6,6 +6,15 @@ sudo apt-get install -y gettext libsndfile1-dev libfftw3-dev libgl-dev
 ./autogen.sh --without-ao
 cd static
 source ./config.sh
+
+OUT_DIR=spectmorph-$PACKAGE_VERSION-x86_64
+rm -rf $OUT_DIR
+mkdir -p $OUT_DIR
+
 cd ..
+SRC_DIR=$PWD
 docker build -t spectmorph-static -f static/Dockerfile .
-docker run -t spectmorph-static misc/statictest.sh
+cd static
+
+docker run -v $SRC_DIR:/output -v $PWD/$OUT_DIR:/data --rm -t spectmorph-static static/staticbuild.sh
+docker run -v $PWD/$OUT_DIR:/data -t spectmorph-static misc/statictest.sh
--- misc/statictest.sh
+++ misc/statictest.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/bash
 set -e
-# test static plugin
-mkdir -p /usr/lib/lv2/spectmorph.lv2
-cp -av lv2/spectmorph_lv2.so.static /usr/lib/lv2/spectmorph.lv2/spectmorph_lv2.so
-cp -av lv2/spectmorph.ttl lv2/manifest.ttl /usr/lib/lv2/spectmorph.lv2
+source static/config.sh
+
+SPECTMORPH_DIR="$HOME/.local/share/spectmorph"
+
+# missing: instruments
+mkdir -p "$SPECTMORPH_DIR"
+cp -av /data/templates /data/fonts "$SPECTMORPH_DIR"
+
 # kxstudio repos for lv2lint
-apt-get install -y wget
+sudo apt-get install -y wget
 wget https://launchpad.net/~kxstudio-debian/+archive/kxstudio/+files/kxstudio-repos_11.1.0_all.deb
-dpkg -i kxstudio-repos_11.1.0_all.deb
-apt-get update
-apt-get install -y lv2lint
+sudo dpkg -i kxstudio-repos_11.1.0_all.deb
+sudo apt-get update
+sudo apt-get install -y lv2lint
+
+# test static plugin
+export LV2_PATH=/data/lv2:/usr/lib/lv2
 lv2lint http://spectmorph.org/plugins/spectmorph
