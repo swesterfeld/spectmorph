8ddaf84e4c655d62eed922c8614f56ac5fcd16b5
Stefan Westerfeld <stefan@space.twc.de>
2024-06-21 10:35:48 +0200

    TESTS: testceventlock: test for ControlEventVector::try_lock()/unlock()
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

---
+++ tests/testceventlock.cc
@@ -0,0 +1,53 @@
+// Licensed GNU LGPL v2.1 or later: http://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "smproject.hh"
+
+using std::vector;
+
+using namespace SpectMorph;
+
+ControlEventVector cv;
+std::atomic<int>   active_threads {0};
+int                max_active_threads;
+
+void
+f (bool lock)
+{
+  for (int i = 0; i < 100; i++)
+    {
+      if (lock)
+        {
+          while (!cv.try_lock())
+            usleep (10 * 1000);
+        }
+      active_threads++;
+      max_active_threads = std::max (max_active_threads, active_threads.load());
+      usleep (1000);
+      active_threads--;
+      if (lock)
+        cv.unlock();
+    }
+}
+
+int
+main()
+{
+  for (auto lock : { false, true })
+    {
+      vector<std::thread> v;
+
+      max_active_threads = 0;
+
+      for (int n = 0; n < 10; n++)
+        v.emplace_back (f, lock);
+
+      for (auto& t : v)
+        t.join();
+      printf ("lock: %s - max_active_threads: %d\n", lock ? "#T" : "#F", max_active_threads);
+
+      if (lock)
+        assert (max_active_threads == 1);
+      else
+        assert (max_active_threads == 10);
+    }
+}
