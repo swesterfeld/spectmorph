317bb9257efe00b4518b090ba2d321c26b79c1bb
Stefan Westerfeld <stefan@space.twc.de>
2024-06-22 20:42:53 +0200

    MISC: macbuild.sh: support asan/ubsan mode
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- misc/macbuild.sh
+++ misc/macbuild.sh
@@ -4,7 +4,8 @@ set -Eeuo pipefail -x
 brew install autoconf-archive automake pkg-config libsndfile jack lv2 fftw libao qt5 scipy libtool
 export PKG_CONFIG_PATH="$(brew --prefix qt@5)/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
 export PATH="$(brew --prefix python)/bin:$PATH"
-./autogen.sh --with-download-instruments
+export UBSAN_OPTIONS=halt_on_error=1
+./autogen.sh --with-download-instruments "$@"
 make -j `sysctl -n hw.ncpu`
 make check
 sudo make install
