f75443060c06ce8e740dc489c791cc83256fa439
Stefan Westerfeld <stefan@space.twc.de>
2024-06-23 14:56:10 +0200

    MISC: macbuild.sh: disable JACK for sanitizer build
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- misc/macbuild.sh
+++ misc/macbuild.sh
@@ -8,7 +8,7 @@ export UBSAN_OPTIONS=halt_on_error=1
 export NPROC=$(sysctl -n hw.ncpu)
 
 if [[ "$1" == "sanitize" ]]; then
-  ./autogen.sh --with-download-instruments --enable-asan --enable-ubsan
+  ./autogen.sh --with-download-instruments --enable-asan --enable-ubsan --without-jack
 
   # plugin build rules do not work for sanitizer builds
   for DIR in 3rdparty lib src tools tests
