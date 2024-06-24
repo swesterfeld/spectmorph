e1c55e8401238818a93890518bca543f371787b9
Stefan Westerfeld <stefan@space.twc.de>
2024-06-24 17:04:24 +0200

    TESTS: post-install-test.sh: support running in wine
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tests/post-install-test.sh
+++ tests/post-install-test.sh
@@ -2,6 +2,21 @@
 
 source ./test-common.sh
 
+if [ "x$1" == "xwine" ]; then
+  export WINE="wine"
+  export WINEDEBUG="-all"
+fi
+
+smrunplan()
+{
+  $WINE ../tools/smrunplan "$@"
+}
+
+smfileedit()
+{
+  $WINE ../tools/smfileedit "$@" | tr -d '\r'
+}
+
 set -e pipefail
 
 TESTS=0
@@ -14,12 +29,12 @@ for PRESET in $(make list-refs | sed 's,ref/,,g;s,\.ref,,g')
 do
   # we need to test without unison for the actual test, however we run in full unison
   # mode without validating the output to trigger asan/ubsan problems
-  ../tools/smrunplan --debug-in-test-program ../data/templates/$PRESET.smplan -q 2>/dev/null
+  smrunplan --debug-in-test-program ../data/templates/$PRESET.smplan -q 2>/dev/null
 
-  DISABLE_UNISON=$(../tools/smfileedit list ../data/templates/$PRESET.smplan |grep 'unison\[0\]=true' |sed 's/true/false/g')
-  ../tools/smfileedit edit ../data/templates/$PRESET.smplan test.smplan $DISABLE_UNISON
+  DISABLE_UNISON=$(smfileedit list ../data/templates/$PRESET.smplan |grep 'unison\[0\]=true' |sed 's/true/false/g')
+  smfileedit edit ../data/templates/$PRESET.smplan test.smplan $DISABLE_UNISON
 
-  ../tools/smrunplan --debug-in-test-program --det-random test.smplan 2>/dev/null |
+  smrunplan --debug-in-test-program --det-random test.smplan 2>/dev/null |
     ../tests/avg_energy.py -1 -1 > $PRESET.tmp
 
   if ! test -f ref/$PRESET.ref; then
