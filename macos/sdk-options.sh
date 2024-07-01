19fd86ba54af3c60283b6888fa8bf3a59deaded4
Stefan Westerfeld <stefan@space.twc.de>
2024-07-01 10:34:42 +0200

    MACOS: sdk-options.sh: use newer SDK for x86_64 (min macOS version is now 11)
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- macos/sdk-options.sh
+++ macos/sdk-options.sh
@@ -1,11 +1,10 @@
+SDK_DIRECTORY="$HOME/MacOSX11.3.sdk"
+SDK_MINVERSION="11"
 if [ x$SDK_TARGET = "xx86_64" ]; then
-  SDK_DIRECTORY="$HOME/MacOSX10.15.sdk"
-  SDK_MINVERSION="10.15"
-  SDK_OPTIONS="-isysroot $SDK_DIRECTORY -mmacosx-version-min=$SDK_MINVERSION -target x86_64-apple-macos10.15"
+  SDK_OPTIONS="-isysroot $SDK_DIRECTORY -mmacosx-version-min=$SDK_MINVERSION -target x86_64-apple-macos11"
   SDK_AUTOCONF_BUILD="--build=x86_64-apple-darwin"
 elif [ x$SDK_TARGET = "xaarch64" ]; then
-  SDK_DIRECTORY="$HOME/MacOSX11.3.sdk"
-  SDK_OPTIONS="-isysroot $SDK_DIRECTORY -mmacosx-version-min=11 -target aarch64-apple-macos11"
+  SDK_OPTIONS="-isysroot $SDK_DIRECTORY -mmacosx-version-min=$SDK_MINVERSION -target aarch64-apple-macos11"
   SDK_AUTOCONF_BUILD="--build=aarch64-apple-darwin"
 else
   :
