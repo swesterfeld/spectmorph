ab95936149b1f969f343a6808f2b94fbc4d22b63
Stefan Westerfeld <stefan@space.twc.de>
2025-06-16 14:47:22 +0200

    LIB: add pitch detection code to library
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

---
+++ lib/smpitchdetect.hh
@@ -0,0 +1,10 @@
+// Licensed GNU LGPL v2.1 or later: http://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "smwavdata.hh"
+
+namespace SpectMorph
+{
+double detect_pitch (WavData& wav_data);
+}
