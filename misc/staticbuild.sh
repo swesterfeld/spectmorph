e9787d29ce7e9dcc257b884ce6983d77521674a0
Stefan Westerfeld <stefan@space.twc.de>
2025-07-03 15:48:27 +0200

    MISC: install libglib2.0-dev for static build
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- misc/staticbuild.sh
+++ misc/staticbuild.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo apt-get install -y gettext libsndfile1-dev libfftw3-dev libgl-dev
+sudo apt-get install -y gettext libsndfile1-dev libfftw3-dev libgl-dev libglib2.0-dev
 ./autogen.sh --without-ao
 cd static
 source ./config.sh
