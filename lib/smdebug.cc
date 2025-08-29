0010ab87c72d5941191f5cb609651cf7164e9cf7
Stefan Westerfeld <stefan@space.twc.de>
2025-08-29 16:25:30 +0200

    LIB: DebugGlobal: avoid global constructors / destructors for debugging
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- lib/smdebug.cc
+++ lib/smdebug.cc
@@ -15,30 +15,52 @@ using namespace SpectMorph;
 using std::string;
 using std::set;
 
-static set<string> active_areas;
-static FILE       *debug_file = NULL;
-static string      debug_filename = "smdebug.log";
-static std::mutex  debug_mutex;
+namespace
+{
+
+struct DebugGlobal
+{
+  set<string> active_areas;
+  FILE       *file = NULL;
+  string      filename = "smdebug.log";
+  std::mutex  mutex;
+
+  void debugv (const char *area, const char *format, va_list vargs);
+};
+
+static DebugGlobal *
+debug_global()
+{
+  static auto instance = new DebugGlobal();   // leak intentionally to avoid global dtor
+  return instance;
+}
+
+};
+
 static std::atomic<int>  have_areas {0};
 
-static inline void
-debugv (const char *area, const char *format, va_list vargs)
+void
+DebugGlobal::debugv (const char *area, const char *format, va_list vargs)
 {
-  std::lock_guard<std::mutex> locker (debug_mutex);
+  std::lock_guard<std::mutex> locker (mutex);
   if (active_areas.find (area) != active_areas.end())
     {
-      if (!debug_file)
+      /* we leak an open FILE here, which usually is not a good idea, the only
+       * reason why we can do it is that debugging is opt-in (via debug areas),
+       * so on an end user installation the file will never be opened
+       */
+      if (!file)
         {
-          char *abs_filename = g_build_filename (sm_get_user_dir (USER_DIR_DATA).c_str(), debug_filename.c_str(), nullptr);
-          debug_file = fopen (abs_filename, "w");
+          char *abs_filename = g_build_filename (sm_get_user_dir (USER_DIR_DATA).c_str(), filename.c_str(), nullptr);
+          file = fopen (abs_filename, "w");
           g_free (abs_filename);
         }
 
-      fprintf (debug_file, "%8s | %s", area, string_vprintf (format, vargs).c_str());
+      fprintf (file, "%8s | %s", area, string_vprintf (format, vargs).c_str());
 
       // hack: avoid fflush for encoder (since it writes lots of lines)
       if (strcmp (area, "encoder") != 0)
-        fflush (debug_file);
+        fflush (file);
     }
 }
 
@@ -55,7 +77,7 @@ Debug::debug (const char *area, const char *fmt, ...)
   va_list ap;
 
   va_start (ap, fmt);
-  debugv (area, fmt, ap);
+  debug_global()->debugv (area, fmt, ap);
   va_end (ap);
 }
 
@@ -69,33 +91,33 @@ sm_debug (const char *fmt, ...)
   va_list ap;
 
   va_start (ap, fmt);
-  debugv ("global", fmt, ap);
+  debug_global()->debugv ("global", fmt, ap);
   va_end (ap);
 }
 
 void
 Debug::enable (const std::string& area)
 {
-  std::lock_guard<std::mutex> locker (debug_mutex);
+  std::lock_guard<std::mutex> locker (debug_global()->mutex);
 
-  active_areas.insert (area);
+  debug_global()->active_areas.insert (area);
   have_areas.store (1);
 }
 
 bool
 Debug::enabled (const std::string& area)
 {
-  std::lock_guard<std::mutex> locker (debug_mutex);
+  std::lock_guard<std::mutex> locker (debug_global()->mutex);
 
-  return active_areas.find (area) != active_areas.end();
+  return debug_global()->active_areas.find (area) != debug_global()->active_areas.end();
 }
 
 void
 Debug::set_filename (const std::string& filename)
 {
-  std::lock_guard<std::mutex> locker (debug_mutex);
+  std::lock_guard<std::mutex> locker (debug_global()->mutex);
 
-  debug_filename = filename;
+  debug_global()->filename = filename;
 }
 
 }
